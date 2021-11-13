7f96fa7a6516854a96db6a8620d648f28855d97b
Brad <brad@Brads-MacBook-Pro.local>
2021-11-13 12:42:47 -0800

    adding some logging to see what url were seeing

--- map/www/add_highlights.js
+++ map/www/add_highlights.js
@@ -5,3 +5,7 @@ Shiny.addCustomMessageHandler("map_markers_added", function(needs_one_arg) {
 	    $(".new_icon").append('<span class="notify-badge notify-badge-new">new</span>');
     }, 500);
 });
+
+console.log("window.location.href: " + window.location.href)
+console.log("window.location.origin: " + window.location.origin)
+
