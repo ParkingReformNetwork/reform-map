db4bcbd49513ae0aad64547760d8dbbef318cdb1
twjordan <twjordan@gmail.com>
2021-11-13 16:25:07 -0800

    move location of details pages

--- map/server.R
+++ map/server.R
@@ -85,7 +85,7 @@ function(input, output, session) {
         mutate(city_state = str_replace_all(paste(city, state, sep="_"), " ", "")) %>%
         select(city_state) %>%
         paste0() %>%
-        paste("https://map.parkingreform.org/parking_map/city_detail/", .,".html", sep="") -> url
+        paste("https://map.parkingreform.org/minimums/detail/", .,".html", sep="") -> url
       HTML(paste(a(city_label, href=url, target="_blank")))
     }
     )
