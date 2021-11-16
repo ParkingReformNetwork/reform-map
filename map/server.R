304f80575dbd8ab1498bf27f15546f33493bf946
Devin MacArthur <jdmacar@pdx.edu>
2021-11-15 21:27:30 -0800

    swaping back

--- map/server.R
+++ map/server.R
@@ -206,7 +206,7 @@ function(input, output, session) {
       
       pal <- colorFactor(
         palette = c("#d7191c", "#fdae61", "#abdda4", "#2b83ba"),
-        c("City Center", "Citywide", "Main Street", "TOD")
+        c("Citywide", "City Center", "Main Street", "TOD")
       )
       
       pal2 <- colorFactor(
