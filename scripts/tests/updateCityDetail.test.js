76afa1675dc6b0269a675594ae9f110fe8c6c5a5
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-05-10 13:45:19 -0600

    Add initial code for update-city-detail.js (#102)
    Unlike the R code, this script properly handles time zones:
    https://github.com/ParkingReformNetwork/mandates-map/issues/97.
    
    A follow up PR will add support for writing a Mustache template to
    create the actual HTML. Once that is done, we can remove the R script.

--- scripts/tests/updateCityDetail.test.js
+++ scripts/tests/updateCityDetail.test.js
@@ -1,6 +1,103 @@
-const { expect, test } = require("@jest/globals");
-const { needsUpdate } = require("../updateCityDetail");
+const { describe, expect, test } = require("@jest/globals");
+const { needsUpdate, parseDatetime } = require("../updateCityDetail");
 
-test("dummy test", () => {
-  expect(needsUpdate()).toBeNull();
+describe("needsUpdate()", () => {
+  test("returns false if everything is older than globalLastUpdated", () => {
+    const entryTime = "May 5, 2023, 8:00:00 AM PDT";
+    const entries = [
+      {
+        "Last updated": entryTime,
+        "Report Last updated": entryTime,
+        "City Last Updated": entryTime,
+      },
+      {
+        "Last updated": "May 4, 2023, 8:00:00 AM PDT",
+        "Report Last updated": entryTime,
+        "City Last Updated": entryTime,
+      },
+    ];
+    const globalLastUpdated = parseDatetime("May 8, 2023, 8:00:00 AM PDT");
+    expect(needsUpdate(entries, globalLastUpdated)).toBe(false);
+  });
+
+  test("returns true if the report has been updated recently", () => {
+    const entryTime = "May 5, 2023, 8:00:00 AM PDT";
+    const entries = [
+      {
+        "Last updated": entryTime,
+        "Report Last updated": "May 10, 2023, 8:00:00 AM PDT",
+        "City Last Updated": entryTime,
+      },
+      {
+        "Last updated": entryTime,
+        "Report Last updated": "May 10, 2023, 8:00:00 AM PDT",
+        "City Last Updated": entryTime,
+      },
+    ];
+    const globalLastUpdated = parseDatetime("May 6, 2023, 8:00:00 AM PDT");
+    expect(needsUpdate(entries, globalLastUpdated)).toBe(true);
+  });
+
+  test("returns true if the city has been updated recently", () => {
+    const entryTime = "May 5, 2023, 8:00:00 AM PDT";
+    const entries = [
+      {
+        "Last updated": entryTime,
+        "Report Last updated": entryTime,
+        "City Last Updated": "May 10, 2023, 8:00:00 AM PDT",
+      },
+      {
+        "Last updated": entryTime,
+        "Report Last updated": entryTime,
+        "City Last Updated": "May 10, 2023, 8:00:00 AM PDT",
+      },
+    ];
+    const globalLastUpdated = parseDatetime("May 6, 2023, 8:00:00 AM PDT");
+    expect(needsUpdate(entries, globalLastUpdated)).toBe(true);
+  });
+
+  test("returns true if at least one of the citations has been updated recently", () => {
+    const entryTime = "May 5, 2023, 8:00:00 AM PDT";
+    const entries = [
+      {
+        "Last updated": entryTime,
+        "Report Last updated": entryTime,
+        "City Last Updated": entryTime,
+      },
+      {
+        "Last updated": "May 10, 2023, 8:00:00 AM PDT",
+        "Report Last updated": entryTime,
+        "City Last Updated": entryTime,
+      },
+    ];
+    const globalLastUpdated = parseDatetime("May 6, 2023, 8:00:00 AM PDT");
+    expect(needsUpdate(entries, globalLastUpdated)).toBe(true);
+  });
+
+  test("can handle different time zones", () => {
+    const entryTime = "May 5, 2023, 8:00:00 AM PDT";
+    const entries = [
+      {
+        "Last updated": "May 6, 2023, 12:00:00 PM PDT",
+        "Report Last updated": entryTime,
+        "City Last Updated": entryTime,
+      },
+      {
+        "Last updated": entryTime,
+        "Report Last updated": entryTime,
+        "City Last Updated": entryTime,
+      },
+    ];
+    // Even though 11 AM is naively earlier than 12 PM, due to time zones, the globalLastUpdated
+    // happens after any of the city updates.
+    let globalLastUpdated = parseDatetime(
+      "May 6, 2023, 11:00:00 AM Pacific/Honolulu",
+      false
+    );
+    expect(needsUpdate(entries, globalLastUpdated)).toBe(false);
+
+    // To be extra sure, we ensure that the time zone would return true.
+    globalLastUpdated = parseDatetime("May 6, 2023, 11:00:00 AM PDT");
+    expect(needsUpdate(entries, globalLastUpdated)).toBe(true);
+  });
 });
--- scripts/updateCityDetail.js
+++ scripts/updateCityDetail.js
@@ -1,3 +1,104 @@
-const needsUpdate = () => null;
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+import fs from "fs/promises";
 
-export { needsUpdate };
+import fetch from "node-fetch";
+import Papa from "papaparse";
+import { DateTime } from "luxon";
+
+const GLOBAL_LAST_UPDATED_FP = "scripts/city_detail_last_updated.txt";
+const TIME_FORMAT = "MMMM d, yyyy, h:mm:ss a z";
+// Luxon does not support abbreviations like EST because they are not standardized:
+//   https://moment.github.io/luxon/#/zones?id=luxon-works-with-time-zones
+const TIME_ZONE_MAPPING = {
+  PDT: "America/Los_Angeles",
+  PST: "America/Los_Angeles",
+};
+
+const parseDatetime = (val, timeZoneAbbreviations = true) => {
+  let cleanedVal = val.replace(/\u202F/g, " ");
+  if (timeZoneAbbreviations) {
+    const tzAbbreviation = cleanedVal.split(" ").pop();
+    const tz = TIME_ZONE_MAPPING[tzAbbreviation];
+    cleanedVal = cleanedVal.replace(tzAbbreviation, tz);
+  }
+  const result = DateTime.fromFormat(cleanedVal, TIME_FORMAT);
+  if (result.invalid) {
+    throw new Error(`Could not parse ${val}: ${result.invalidExplanation}`);
+  }
+  return result;
+};
+
+const fetchData = async () => {
+  const response = await fetch(
+    "https://area120tables.googleapis.com/link/aUJhBkwwY9j1NpD-Enh4WU/export?key=aasll5u2e8Xf-jxNNGlk3vbnOYcDsJn-JbgeI3z6IkPk8z5CxpWOLEp5EXd8iMF_bc"
+  );
+  const rawData = await response.text();
+  return Papa.parse(rawData, { header: true }).data;
+};
+
+const needsUpdate = (cityEntries, globalLastUpdated) => {
+  const lastUpdatedDates = cityEntries.map((row) =>
+    parseDatetime(row["Last updated"])
+  );
+  const reportLastUpdated = parseDatetime(
+    cityEntries[0]["Report Last updated"]
+  );
+  const cityLastUpdated = parseDatetime(cityEntries[0]["City Last Updated"]);
+  const maxLastUpdated = DateTime.max(
+    ...lastUpdatedDates,
+    reportLastUpdated,
+    cityLastUpdated
+  );
+  return maxLastUpdated >= globalLastUpdated;
+};
+
+const processCity = (cityState, entries, globalLastUpdated) => {
+  if (!needsUpdate(entries, globalLastUpdated)) {
+    console.log(`Skipping ${cityState}`);
+    return;
+  }
+
+  console.log(`Updating ${cityState}`);
+};
+
+const updateLastUpdatedFile = async () => {
+  console.log(
+    `Updating ${GLOBAL_LAST_UPDATED_FP} with today's date and time zone`
+  );
+  const currentDatetime = DateTime.local().setZone("local");
+  const formatted = currentDatetime.toFormat(TIME_FORMAT);
+  await fs.writeFile(GLOBAL_LAST_UPDATED_FP, formatted, "utf-8");
+};
+
+const updateCities = async () => {
+  const [rawGlobalLastUpdated, data] = await Promise.all([
+    fs.readFile(GLOBAL_LAST_UPDATED_FP, "utf-8"),
+    fetchData(),
+  ]);
+  const globalLastUpdated = parseDatetime(rawGlobalLastUpdated, false);
+
+  const cityStateMap = {};
+  data.forEach((row) => {
+    if (!row.City || !row.State) {
+      return;
+    }
+    const cityState = `${row.City}_${row.State}`;
+    if (!cityStateMap[cityState]) {
+      cityStateMap[cityState] = [];
+    }
+    cityStateMap[cityState].push(row);
+  });
+
+  Object.entries(cityStateMap).forEach(([cityState, entries]) =>
+    processCity(cityState, entries, globalLastUpdated)
+  );
+
+  await updateLastUpdatedFile();
+};
+
+if (process.env.NODE_ENV !== "test") {
+  updateCities().catch((error) => console.error(error));
+}
+
+export { needsUpdate, parseDatetime };
