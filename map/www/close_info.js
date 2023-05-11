ec5c0981dc00810bba8568691fc10eb6bc98ef41
Eric Arellano <ericarellano@me.com>
2023-05-11 07:03:50 -0600

    Use arrow functions

--- map/www/close_info.js
+++ map/www/close_info.js
@@ -1,7 +1,7 @@
 /* global window, document */
-window.onload = function closeInfo() {
+window.onload = () => {
   const clickInfo = document.getElementById("click_info");
-  document.onclick = function handleClick(e) {
+  document.onclick = (e) => {
     if (e.target.id !== "click_info") {
       clickInfo.style.display = "none";
     }
