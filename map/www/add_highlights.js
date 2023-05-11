d44b064aed87b3ccbb76bf3ffe10267d7ba77b05
Eric Arellano <ericarellano@me.com>
2023-05-11 06:52:38 -0600

    Autoformat map/www with Prettier

--- map/www/add_highlights.js
+++ map/www/add_highlights.js
@@ -1,11 +1,14 @@
 // This function adds custom text badges to the markers
-Shiny.addCustomMessageHandler("map_markers_added", function(needs_one_arg) {
-	setTimeout(function(){
-	    $(".highlighed_icon").append('<span class="notify-badge notify-badge-highlight">!!!</span>');
-	    $(".new_icon").append('<span class="notify-badge notify-badge-new">new</span>');
-    }, 500);
+Shiny.addCustomMessageHandler("map_markers_added", function (needs_one_arg) {
+  setTimeout(function () {
+    $(".highlighed_icon").append(
+      '<span class="notify-badge notify-badge-highlight">!!!</span>'
+    );
+    $(".new_icon").append(
+      '<span class="notify-badge notify-badge-new">new</span>'
+    );
+  }, 500);
 });
 
-console.log("window.location.href: " + window.location.href)
-console.log("window.location.origin: " + window.location.origin)
-
+console.log("window.location.href: " + window.location.href);
+console.log("window.location.origin: " + window.location.origin);
