1392eada10012fb84ec77487bf2d0420dc9fd06c
tunglinn <tunglin@yahoo.com>
2023-06-22 19:56:26 -0700

    added base leaflet map

---
+++ scripts/setUpSite.js
@@ -0,0 +1,27 @@
+import { Map, TileLayer } from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+const base = new TileLayer(
+  "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}",
+  {
+    attribution:
+      'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    subdomains: "abcd",
+    minZoom: 0,
+    maxZoom: 20,
+    ext: "png",
+  }
+);
+
+const setUpSite = async () => {
+  const map = new Map("map", {
+    layers: [base],
+  });
+  map.setView([40.9437, -78.9709], 12); // latitude, longitude
+
+  map.attributionControl.setPrefix(
+    'created by <a style="padding: 0 3px 0 3px; color:#fafafa; background-color: #21ccb9;" href=http://www.geocadder.bg/en/>GEOCADDER</a>'
+  );
+};
+
+export default setUpSite;
