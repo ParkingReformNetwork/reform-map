864d8bef4a08f20d8bcc3f8cdb9d1060793b769f
tunglinn <tunglin@yahoo.com>
2023-06-29 22:47:20 -0700

    added info button popup

--- scripts/setUpSite.js
+++ scripts/setUpSite.js
@@ -15,8 +15,38 @@ const base = new TileLayer(
   }
 );
 
+const setUpAbout = () => {
+  const aboutElement = document.querySelector(".about-text-popup");
+  const infoButton = document.querySelector(".info-icon");
+
+  infoButton.addEventListener("click", () => {
+    aboutElement.style.display =
+      aboutElement.style.display !== "block" ? "block" : "none";
+  });
+
+  // closes window on clicks outside the info popup
+  window.addEventListener("click", function (event) {
+    if (
+      !infoButton.contains(event.target) &&
+      aboutElement.style.display == "block" &&
+      !aboutElement.contains(event.target)
+    ) {
+      aboutElement.style.display = "none";
+      infoButton.classList.toggle("active");
+    }
+  });
+
+  // Note that the close element will only render when the about text popup is rendered.
+  // So, it only ever makes sense for a click to close.
+  document.querySelector(".about-close").addEventListener("click", () => {
+    aboutElement.style.display = "none";
+    infoButton.classList.toggle("active");
+  });
+};
+
 const setUpSite = async () => {
   setUpIcons();
+  setUpAbout();
 
   const map = new Map("map", {
     layers: [base],
