8b93e89a5c4f41f68a3c0780253dbaf90c744422
tunglinn <tunglin@yahoo.com>
2023-07-17 17:27:36 -0700

    add tests

--- playwright.config.js
+++ playwright.config.js
@@ -5,16 +5,14 @@ import { defineConfig } from "@playwright/test";
 // NODE_ENV is used in updateCityDetail.js to prevent the main function from running when it's being imported for testing
 process.env.NODE_ENV = "test";
 
-/* uncomment when we add server tests
+
 // Note that we may run tests by using Parcel (development) vs running on the `dist` folder.
 const port = process.env.PORT || 1234;
 const webServerCommand = port === 1234 ? "start" : "serve-dist";
 
 const baseURL = `http://127.0.0.1:${port}`;
-*/
 export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
-  /*  uncomment when we add server tests
   use: {
     baseURL,
   },
@@ -22,5 +20,5 @@ export default defineConfig({
     command: `npm run ${webServerCommand}`,
     url: baseURL,
     reuseExistingServer: !process.env.CI
-  },*/
+  },
 });
---
+++ tests/setUpSite.test.js
@@ -0,0 +1,34 @@
+/* global document, navigator */
+import { expect, test } from "@playwright/test";
+
+test("about popup can be opened and closed", async ({ page }) => {
+    await page.goto("");
+  
+    const aboutIcon = ".info-icon";
+  
+    const aboutIsVisible = async () => {
+        return await page.$eval(
+        ".about-text-popup",
+        (el) => el.style.display === "block"
+        );
+    };
+
+    // before click
+    expect(await aboutIsVisible()).toBe(false);
+    
+    // click about icon (open popup)
+    await page.click(aboutIcon);
+    expect(await aboutIsVisible()).toBe(true);
+
+    // click about icon (close popup)
+    await page.click(aboutIcon);
+    expect(await aboutIsVisible()).toBe(false);
+
+    // click about icon (open popup)
+    await page.click(aboutIcon);
+    expect(await aboutIsVisible()).toBe(true);
+    
+    // click x icon in popup
+    await page.click(".about-close");
+    expect(await aboutIsVisible()).toBe(false);
+  });
