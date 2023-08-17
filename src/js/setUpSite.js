6c3e4edc9dc151e102038d20691f0e76056bee4c
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-08-16 18:50:12 -0700

    Move setUpSite.js to the correct folder (#179)
    Also refactor the JavaScript to be more idiomatic by using constants and
    a helper function for creating the map.

--- src/js/setUpSite.js
+++ src/js/setUpSite.js
@@ -1,8 +1,9 @@
 import { Map, TileLayer, CircleMarker } from "leaflet";
 import "leaflet/dist/leaflet.css";
-import setUpIcons from "../src/js/fontAwesome";
 
-const base = new TileLayer(
+import setUpIcons from "./fontAwesome";
+
+const BASE_LAYER = new TileLayer(
   "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}",
   {
     attribution:
@@ -14,22 +15,34 @@ const base = new TileLayer(
   }
 );
 
+const SCOPE_TO_COLOR = {
+  Regional: "#7b3294",
+  "City Center": "#fdae61",
+  Citywide: "#d7191c",
+  "Main Street": "#abdda4",
+  TOD: "#2b83ba",
+};
+
+const createMap = () => {
+  const map = new Map("map", {
+    layers: [BASE_LAYER],
+  });
+  map.setView([43.2796758, -96.7449732], 4); // Set default view (lat, long) to United States
+  map.attributionControl.setPrefix(
+    'created by <a style="padding: 0 3px 0 3px; color:#fafafa; background-color: #21ccb9;" href=http://www.geocadder.bg/en/>GEOCADDER</a>'
+  );
+  return map;
+};
+
 const setUpCityPointsLayer = async (map) => {
-  const data = await import("../map/tidied_map_data.csv");
-  const scope = {
-    Regional: "#7b3294",
-    "City Center": "#fdae61",
-    Citywide: "#d7191c",
-    "Main Street": "#abdda4",
-    TOD: "#2b83ba",
-  };
+  const data = await import("../../map/tidied_map_data.csv");
   data.forEach((mandate) => {
     new CircleMarker([mandate.lat, mandate.long], {
       radius: 7,
       stroke: true,
       weight: 0.9,
       color: "#FFFFFF",
-      fillColor: scope[mandate.magnitude_encoded],
+      fillColor: SCOPE_TO_COLOR[mandate.magnitude_encoded],
       fillOpacity: 1,
     }).addTo(map);
   });
@@ -37,16 +50,7 @@ const setUpCityPointsLayer = async (map) => {
 
 const setUpSite = async () => {
   setUpIcons();
-
-  const map = new Map("map", {
-    layers: [base],
-  });
-  map.setView([43.2796758, -96.7449732], 4); // Set default view (lat, long) to United States
-
-  map.attributionControl.setPrefix(
-    'created by <a style="padding: 0 3px 0 3px; color:#fafafa; background-color: #21ccb9;" href=http://www.geocadder.bg/en/>GEOCADDER</a>'
-  );
-
+  const map = createMap();
   await setUpCityPointsLayer(map);
 };
 
