70549d42129aca043aad7ea6b5df14433eb3274a
Eric Arellano <ericarellano@me.com>
2023-08-19 07:06:04 -0700

    Set up selectize!

--- src/js/search.js
+++ src/js/search.js
@@ -1,18 +1,18 @@
 import jquery from "jquery";
+import "@selectize/selectize";
+import "@selectize/selectize/dist/css/selectize.css";
 
-// jQuery is not set up with ES6 modules and Parcel by default.
-// This defines the $ symbol. See
+// This exposes jQuery and the $ symbol. See
 // https://stackoverflow.com/a/47984928.
 window.$ = window.jQuery = jquery;
 
 const setUpSearch = () => {
-  $(".city-search");
-//   $(".city-search").selectize({
-//     options: [
-//       { key: 1, title: "opt1" },
-//       { key: 2, title: "opt2" },
-//     ],
-//   });
+  $(".city-search").selectize({
+    options: [
+      { key: 1, title: "opt1" },
+      { key: 2, title: "opt2" },
+    ],
+  });
 };
 
 export default setUpSearch;
