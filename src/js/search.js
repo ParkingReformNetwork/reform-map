54ed326a597c97890dbeea45e707a7de9ef10e42
Eric Arellano <ericarellano@me.com>
2023-08-19 19:40:34 -0700

    Fix dragging from dropdown

--- src/js/search.js
+++ src/js/search.js
@@ -22,12 +22,17 @@ const setUpSearch = (map, data) => {
     const desc = `${entry.city}, ${entry.state}`;
     return { value: desc, text: desc };
   });
+
+  // Convert `<select>` into selectize component.
   $(".city-search").selectize({
     options: cities,
     placeholder: "City search",
   });
 
-  // TODO: turn off dragging with the dropdown.
+  // Turn off dragging of map with option dropdown.
+  const dropdown = document.querySelector(".city-search.selectize-dropdown");
+  DomEvent.on(dropdown, "mousedown", DomEvent.stopPropagation);
+  DomEvent.on(dropdown, "click", DomEvent.stopPropagation);
 };
 
 export default setUpSearch;
