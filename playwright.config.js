95781cd4d04c3c92b5fc431150769d7e0c332c0e
Eric Arellano <ericarellano@me.com>
2023-08-21 18:18:56 -0700

    Add test for search

--- playwright.config.js
+++ playwright.config.js
@@ -1,26 +1,23 @@
 import { defineConfig } from "@playwright/test";
 
-// jest automatically sets process.env.NODE_ENV = 'test'
-// playwright does not
-// NODE_ENV is used in updateCityDetail.js to prevent the main function from running when it's being imported for testing
+// We set this env var so our scripts can avoid running their `main`
+// functions during testing.
 process.env.NODE_ENV = "test";
 
-/* uncomment when we add server tests
 // Note that we may run tests by using Parcel (development) vs running on the `dist` folder.
 const port = process.env.PORT || 1234;
 const webServerCommand = port === 1234 ? "start" : "serve-dist";
 
 const baseURL = `http://127.0.0.1:${port}`;
-*/
+
 export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
-  /*  uncomment when we add server tests
   use: {
     baseURL,
   },
   webServer: {
     command: `npm run ${webServerCommand}`,
     url: baseURL,
-    reuseExistingServer: !process.env.CI
-  },*/
+    reuseExistingServer: !process.env.CI,
+  },
 });
