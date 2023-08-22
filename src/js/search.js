d29713308a2c54669a6d3d10a41a46ae926ec2ba
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-08-21 17:25:19 -0700

    Search impacts what cities show up on the map (#202)
    Closes https://github.com/ParkingReformNetwork/reform-map/issues/195.
    
    This has a time complexity of O(n) because we have to iterate through
    every element to determine whether it should be added or removed. With n
    == 1600, this is extremely fast locally for me.

--- src/js/search.js
+++ src/js/search.js
@@ -6,14 +6,30 @@ import "@selectize/selectize/dist/css/selectize.css";
 // https://stackoverflow.com/a/47984928.
 window.$ = window.jQuery = jquery;
 
-const setUpSearch = (data) => {
-  const cities = data.map((entry) => {
-    const desc = `${entry.city}, ${entry.state}`;
-    return { value: desc, text: desc };
+// Using `=>` twice allows for partial application. The caller can pre-set the `map` and
+// `citiesToMarkers` arguments. That results in a function that takes only one value
+// for `currentlySelected`, which is what selectize expects with its `onChange` argument.
+const onChange = (map, citiesToMarkers) => (currentlySelected) => {
+  const selectedSet = new Set(currentlySelected);
+  Object.entries(citiesToMarkers).forEach(([cityState, marker]) => {
+    if (selectedSet.size === 0 || selectedSet.has(cityState)) {
+      marker.addTo(map);
+    } else {
+      marker.remove();
+    }
   });
+};
+
+const setUpSearch = (map, citiesToMarkers) => {
+  const cities = Object.keys(citiesToMarkers).map((cityState) => ({
+    value: cityState,
+    text: cityState,
+  }));
   $(".city-search").selectize({
     options: cities,
     placeholder: "City search",
+    onChange: onChange(map, citiesToMarkers),
+    maxOptions: 3000,
   });
 };
 
--- src/js/setUpSite.js
+++ src/js/setUpSite.js
@@ -1,4 +1,4 @@
-import { Map, TileLayer, CircleMarker, Control, DomUtil } from "leaflet";
+import { Map, TileLayer, CircleMarker } from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 import setUpIcons from "./fontAwesome";
@@ -36,8 +36,26 @@ const createMap = () => {
   return map;
 };
 
-const setUpCityPointsLayer = (map, data) => {
-  data.forEach((entry) => {
+/**
+ * Read the CSV and return an object with `City, State` as the key and the original entry as the value.
+ */
+const readData = async () => {
+  const data = await import("../../map/tidied_map_data.csv");
+  const sortedData = data.sort((a, b) =>
+    `${a.city}, ${a.state}`.localeCompare(`${b.city}, ${b.state}`)
+  );
+  return sortedData.reduce((acc, entry) => {
+    const cityState = `${entry.city}, ${entry.state}`;
+    acc[cityState] = entry;
+    return acc;
+  }, {});
+};
+
+/**
+ * Returns an object mapping cityState to its CircleMarker.
+ */
+const createCityMarkers = (map, data) =>
+  Object.entries(data).reduce((acc, [cityState, entry]) => {
     const marker = new CircleMarker([entry.lat, entry.long], {
       radius: 7,
       stroke: true,
@@ -46,19 +64,21 @@ const setUpCityPointsLayer = (map, data) => {
       fillColor: SCOPE_TO_COLOR[entry.magnitude_encoded],
       fillOpacity: 1,
     });
-    marker.bindTooltip(`${entry.city}, ${entry.state}`);
+    acc[cityState] = marker;
+
+    marker.bindTooltip(cityState);
     marker.addTo(map);
-  });
-};
+    return acc;
+  }, {});
 
 const setUpSite = async () => {
   setUpIcons();
   const map = createMap();
   addLegend(map, SCOPE_TO_COLOR);
 
-  const data = await import("../../map/tidied_map_data.csv");
-  setUpSearch(data);
-  setUpCityPointsLayer(map, data);
+  const data = await readData();
+  const citiesToMarkers = createCityMarkers(map, data);
+  setUpSearch(map, citiesToMarkers);
 };
 
 export default setUpSite;
