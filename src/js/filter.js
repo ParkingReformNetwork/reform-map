af629d0d21ec57ea6fc0dde4294553720de7d385
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-09-05 19:31:02 -0700

    DRY filter functionality (#222)

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,43 +1,47 @@
 /* global document, window */
 
+/**
+ * Helper function to iterate over every city and either remove it or add it,
+ * depending on the result of filterFn for that city.
+ *
+ * @param filterFn: a function of (string) => boolean. That is, a function that takes
+ *    one argument for the cityState name, e.g. 'Boston, MA', and returns true if it
+ *    should be displayed on the map.
+ */
+const changeSelectedMarkers = (markerGroup, citiesToMarkers, filterFn) => {
+  Object.entries(citiesToMarkers).forEach(([cityState, marker]) => {
+    if (filterFn(cityState)) {
+      marker.addTo(markerGroup);
+    } else {
+      marker.removeFrom(markerGroup);
+    }
+  });
+};
+
+// The double => is "partial application".
+const onScopeFilterChange = (markerGroup, citiesToMarkers, data) => () => {
+  const selected = new Set(
+    [...document.querySelectorAll(".filter--scope :checked")].map(
+      (option) => option.value
+    )
+  );
+  changeSelectedMarkers(markerGroup, citiesToMarkers, (cityState) =>
+    data[cityState]["report_magnitude"]
+      .split(",")
+      .some((scope) => selected.has(scope))
+  );
+};
+
 const setUpFilter = (markerGroup, citiesToMarkers, data) => {
   const filter = document.querySelector(".filter--scope");
-  const priorSelection = [
-    "Regional",
-    "Citywide",
-    "City Center",
-    "Transit Oriented",
-    "Main Street",
-  ];
-
-  const onScopeFilterChange = (markerGroup, citiesToMarkers, data) => () => {
-    const selected = new Set(
-      [...document.querySelectorAll(".filter--scope :checked")].map(
-        (option) => option.value
-      )
-    );
-    Object.entries(citiesToMarkers).forEach(([cityState, marker]) => {
-      if (
-        data[cityState]["report_magnitude"]
-          .split(",")
-          .some((scope) => selected.has(scope))
-      ) {
-        marker.addTo(markerGroup);
-      } else {
-        marker.removeFrom(markerGroup);
-      }
-    });
-  };
-
-  // makes all options selected
+  // Pre-select all options.
   filter.querySelectorAll("option").forEach((option) => {
-    option.selected = priorSelection.includes(option.value);
+    option.selected = true;
   });
-
   filter.addEventListener(
     "change",
     onScopeFilterChange(markerGroup, citiesToMarkers, data)
   );
 };
 
-export default setUpFilter;
+export { changeSelectedMarkers, setUpFilter };
