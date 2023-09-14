27fb01e5c3a4493b9500c709e0508e8cb8359893
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-09-13 23:05:37 -0600

    Connect search and filter functionality (#235)
    Before, each filter was being traeted as its own separate thing, without
    paying attention to the other filters and the search. They would each
    call `changeSelectedMarkers` with their own `filterFn`. So if you
    changed one filter, it would ignore the other filters and override their
    prior filtering.
    
    Instead, when deciding whether to show a city, we need to consider _all_
    the filters. The city must match every filter.
    
    ## Search vs filter
    
    I wasn't sure how we should handle search vs. the filter mechanism. If
    you have selected a city in the search, but it doesn't match the
    filters, what should we do?
    
    ChatGPT helped convince me that search should take priority. If you
    actively choose a city, then the least confusing UX is to still show
    that city, regardless of filters.
    
    ## Punts on population slider
    
    I was having a tough time figuring out how to hook up the code to the
    filter. I want to refactor the code first. So this PR punts on that
    file.

--- src/js/filter.ts
+++ src/js/filter.ts
@@ -1,19 +1,56 @@
 /* global document, window */
 
+import type Choices from "choices.js";
 import type { CircleMarker, FeatureGroup } from "leaflet";
 import type { CityId, CityEntry } from "./types";
 
 /**
+ * Return true if the city should be rendered on the map.
+ *
+ * Note that search takes priority. If certain cities are selected via
+ * search, they should be shown regardless of the filters.
+ */
+const shouldBeRendered = (
+  cityState: CityId,
+  entry: CityEntry,
+  searchElement: Choices
+): boolean => {
+  const searchChosen = new Set(searchElement.getValue(true) as string[]);
+  if (searchChosen.size > 0) {
+    return searchChosen.has(cityState);
+  }
+
+  // Else, search is not used and the filters should apply.
+  const getSelected = (cls: string): Set<string> =>
+    new Set(
+      Array.from(document.querySelectorAll(cls)).map(
+        (option: HTMLInputElement) => option.value
+      )
+    );
+
+  const scopeSelected = getSelected(".filter--scope :checked");
+  const isScope = entry["report_magnitude"]
+    .split(",")
+    .some((scope) => scopeSelected.has(scope));
+
+  return isScope;
+};
+
+/**
  * Helper function to iterate over every city and either remove it or add it,
- * depending on the result of filterFn for that city.
+ * based on the search and filter values.
+ *
+ * This should be used with an event listener for each filter and search, whenever
+ * their values change.
  */
 const changeSelectedMarkers = (
   markerGroup: FeatureGroup,
   citiesToMarkers: Record<CityId, CircleMarker>,
-  filterFn: (cityState: CityId) => boolean
+  data: Record<CityId, CityEntry>,
+  searchElement: Choices
 ) => {
   Object.entries(citiesToMarkers).forEach(([cityState, marker]) => {
-    if (filterFn(cityState)) {
+    if (shouldBeRendered(cityState, data[cityState], searchElement)) {
       marker.addTo(markerGroup);
     } else {
       // @ts-ignore the API allows passing a LayerGroup, but the type hint doesn't show this.
@@ -22,40 +59,17 @@ const changeSelectedMarkers = (
   });
 };
 
-// The double => is "partial application".
-const onScopeFilterChange =
-  (
-    markerGroup: FeatureGroup,
-    citiesToMarkers: Record<CityId, CircleMarker>,
-    data: Record<CityId, CityEntry>
-  ) =>
-  (): void => {
-    const selected = new Set(
-      Array.from(document.querySelectorAll(".filter--scope :checked")).map(
-        (option: HTMLInputElement) => option.value
-      )
-    );
-    changeSelectedMarkers(markerGroup, citiesToMarkers, (cityState) =>
-      data[cityState]["report_magnitude"]
-        .split(",")
-        .some((scope) => selected.has(scope))
-    );
-  };
-
 const setUpFilter = (
   markerGroup: FeatureGroup,
   citiesToMarkers: Record<CityId, CircleMarker>,
-  data: Record<CityId, CityEntry>
+  data: Record<CityId, CityEntry>,
+  searchElement: Choices
 ): void => {
-  const filter = document.querySelector(".filter--scope");
-  // Pre-select all options.
-  filter.querySelectorAll("option").forEach((option) => {
-    option.selected = true;
-  });
-  filter.addEventListener(
-    "change",
-    onScopeFilterChange(markerGroup, citiesToMarkers, data)
-  );
+  document
+    .querySelector(".filter--scope")
+    .addEventListener("change", () =>
+      changeSelectedMarkers(markerGroup, citiesToMarkers, data, searchElement)
+    );
 };
 
-export { changeSelectedMarkers, setUpFilter };
+export { changeSelectedMarkers, setUpFilter, shouldBeRendered };
