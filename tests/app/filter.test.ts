a982c6d561d3b662d50e364575f4ab1a74a5f69a
Tung Lin <86996158+tunglinn@users.noreply.github.com>
2023-09-19 15:15:20 -0700

    Change filter for better UX (#233)
    Resolves #228 
    
    User no longer has to use shift or ctrl/cmd for multi-selection.
    Instead, clicks will update current selection by removing or adding 1
    option.
    
    
    ![image](https://github.com/ParkingReformNetwork/reform-map/assets/86996158/94bc0731-853d-4b41-b819-1c2bc8ad7c16)
    
    ---------
    
    Co-authored-by: tunglinn <tunglin@yahoo.com>
    Co-authored-by: Eric-Arellano <Eric-Arellano@users.noreply.github.com>

--- tests/app/filter.test.ts
+++ tests/app/filter.test.ts
@@ -14,25 +14,61 @@ test("scope filter updates markers", async ({ page }) => {
     "Main Street/Special": 33,
   };
 
-  const checkScope = async (
-    scopeType: string | Array<string>
-  ): Promise<void> => {
-    await page.selectOption(".filter--scope", scopeType);
+  const checkScope = async (scopeType: string): Promise<void> => {
+    const citiesBefore = await getNumCities(page);
+    const optionElement = await page.$(
+      `.filter--scope option:has-text("${scopeType}")`
+    );
 
-    const min = Array.isArray(scopeType)
-      ? scopeType.reduce((sum, key) => sum + (scopeMin[key] || 0), 0)
-      : scopeMin[scopeType] || 0;
+    // Click the option element
+    if (optionElement) {
+      await optionElement.click();
+    }
     const cities = await getNumCities(page);
 
-    expect(cities >= min && cities < min * 1.5).toBe(true);
+    // check if all options are deselected
+    const areAllOptionsDeselected = await page.evaluate((): boolean => {
+      const selectElement = document.querySelector(
+        ".filter--scope"
+      ) as HTMLSelectElement;
+      const options = Array.from(selectElement.options);
+      return options.every((option) => !option.selected);
+    });
+
+    if (areAllOptionsDeselected) {
+      expect(cities === 0).toBe(true);
+    } else {
+      const isSelected = await page.$eval(
+        ".filter--scope",
+        (selectElement: HTMLSelectElement, scope: string): boolean => {
+          const options = Array.from(selectElement.options);
+          const option = options.find((opt) => opt.textContent === scope);
+          return option ? option.selected : false;
+        },
+        scopeType
+      );
+      const change = Array.isArray(scopeType)
+        ? scopeType.reduce((sum, key) => sum + (scopeMin[key] || 0), 0)
+        : scopeMin[scopeType] || 0;
+
+      if (isSelected) {
+        const min = citiesBefore + change;
+        expect(cities >= min && cities < min * 1.5).toBe(true);
+      } else {
+        const max = citiesBefore - change;
+        expect(cities <= max && cities >= max * 0.5).toBe(true);
+      }
+    }
   };
 
-  await checkScope("Regional");
+  await checkScope("Regional"); // clicking on an option will remove it
   await checkScope("Citywide");
   await checkScope("City Center/Business District");
   await checkScope("Transit Oriented");
+  await checkScope("Transit Oriented");
   await checkScope("Main Street/Special");
-  await checkScope(["Transit Oriented", "City Center/Business District"]);
+  await checkScope("Transit Oriented");
+  await checkScope("Citywide");
 });
 
 test("population filter updates markers", async ({ page }) => {
