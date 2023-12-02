4bc6e75447f8c2c63b4446bd1427ef1246f61c94
Eric Arellano <ericarellano@me.com>
2023-12-01 19:06:37 -0500

    Update README & remove R code

--- map/www/close_info.js
+++
@@ -1,9 +0,0 @@
-/* global window, document */
-window.onload = () => {
-  const clickInfo = document.getElementById("click_info");
-  document.onclick = (e) => {
-    if (e.target.id !== "click_info") {
-      clickInfo.style.display = "none";
-    }
-  };
-};
