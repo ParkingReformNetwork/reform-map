14c3b971bdb97caa2dce98f51ca5303b69240d4e
Eric Arellano <ericarellano@me.com>
2024-07-30 08:50:54 -0400

    Set up search popup

--- src/js/search.ts
+++ src/js/search.ts
@@ -1,7 +1,42 @@
 import Choices from "choices.js";
 import "choices.js/public/assets/styles/choices.css";
 
+import Observable from "./Observable";
 import { PlaceFilterManager } from "./FilterState";
+
+function updateSearchPopupUI(isVisible: boolean) {
+  const popup = document.querySelector<HTMLElement>("#search-popup");
+  const icon = document.querySelector(".header-search-icon-container");
+  if (!popup || !icon) return;
+  popup.hidden = !isVisible;
+  icon.ariaExpanded = isVisible.toString();
+}
+
+function initSearchPopup(): void {
+  const isVisible = new Observable<boolean>(false);
+  isVisible.subscribe(updateSearchPopupUI);
+
+  const popup = document.querySelector("#search-popup");
+  const icon = document.querySelector(".header-search-icon-container");
+
+  icon.addEventListener("click", () => {
+    isVisible.setValue(!isVisible.getValue());
+  });
+
+  // Clicks outside the popup close it.
+  window.addEventListener("click", (event) => {
+    if (
+      isVisible.getValue() === true &&
+      event.target instanceof Element &&
+      !icon?.contains(event.target) &&
+      !popup?.contains(event.target)
+    ) {
+      isVisible.setValue(false);
+    }
+  });
+
+  isVisible.initialize();
+}
 
 export default function initSearch(filterManager: PlaceFilterManager): void {
   const places = Object.keys(filterManager.entries).map((placeId) => ({
@@ -24,4 +59,7 @@ export default function initSearch(filterManager: PlaceFilterManager): void {
   htmlElement.addEventListener("change", () => {
     filterManager.update({ searchInput: choices.getValue(true) as string[] });
   });
+
+  // Also set up the popup.
+  initSearchPopup();
 }
