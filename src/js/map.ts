5ce59896b4d7be3c5a6d8952c405f053513213f1
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-08-01 22:48:57 -0400

    Improve marker styling (#426)
    Improvements:
    
    * Increases marker size when you zoom in
    * Differentiates no-mandates places from other places
         * red vs orange
         * no-mandates guaranteed to be on top (z-index)
         * no-mandates have 2px larger radius
    * turns off zoom past level 3 (default), since it is too big to be
    useful and it does not render Asia correctly until you zoom to the left
    
    Orange was chosen to be accessible for color blindness; it contrasts
    well with the red on grayscale.
    
    <img width="322" alt="Screenshot 2024-08-01 at 10 43 43 PM"
    src="https://github.com/user-attachments/assets/53cdefc5-ef0a-4502-b7c9-37a7de61dac6">
    
    <img width="330" alt="Screenshot 2024-08-01 at 10 44 12 PM"
    src="https://github.com/user-attachments/assets/10d66064-fdc7-488c-8496-9473c91296da">

--- src/js/map.ts
+++ src/js/map.ts
@@ -1,6 +1,10 @@
 import { Map, TileLayer } from "leaflet";
 import "leaflet/dist/leaflet.css";
 
+export const NO_MANDATES_MARKERS_PANE = "noMandatesMarkers";
+const MIN_ZOOM = 3;
+const MAX_ZOOM = 10;
+
 const BASE_LAYER = new TileLayer(
   "https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.png",
   {
@@ -9,8 +13,8 @@ const BASE_LAYER = new TileLayer(
         &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a>
         &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a>`,
     subdomains: "abcd",
-    minZoom: 0,
-    maxZoom: 10,
+    minZoom: MIN_ZOOM,
+    maxZoom: MAX_ZOOM,
   },
 );
 
@@ -19,6 +23,7 @@ export default function createMap(): Map {
     layers: [BASE_LAYER],
     worldCopyJump: true,
   });
+
   // Set default view show all the US on mobile. While this is fairly zoomed out,
   // the main purpose of the map on initial load is to tell the narrative that parking
   // reform is popular, i.e. there are a lot of dots. Search, filter, and table view are
@@ -27,5 +32,10 @@ export default function createMap(): Map {
   map.attributionControl.setPrefix(
     '<a href="https://parkingreform.org/support/">Parking Reform Network</a>',
   );
+
+  // Workaround for no mandates markers appearing at the top of the map
+  // https://stackoverflow.com/a/49501320
+  map.createPane(NO_MANDATES_MARKERS_PANE);
+  map.getPane(NO_MANDATES_MARKERS_PANE).style.zIndex = "900";
   return map;
 }
