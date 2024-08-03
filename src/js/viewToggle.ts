6e32aa60b40390dddd2e5c971b9288db830cb5be
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-08-02 21:50:11 -0400

    Add toggle for table vs map view (#430)
    We can now toggle between these two views:
    
    <img width="328" alt="Screenshot 2024-08-02 at 9 46 33 PM"
    src="https://github.com/user-attachments/assets/3c9a7a73-0082-4786-8151-7f497397a654">
    <img width="325" alt="Screenshot 2024-08-02 at 9 46 44 PM"
    src="https://github.com/user-attachments/assets/0e525b78-8e8c-49bd-ae53-614427d1139d">
    
    The toggle is hidden so that users cannot yet access the control until
    we're ready. But this gets the first part of the change in-place.

---
+++ src/js/viewToggle.ts
@@ -0,0 +1,49 @@
+import Observable from "./Observable";
+
+type ViewState = "map" | "table";
+
+type ViewStateObservable = Observable<ViewState>;
+
+function updateUI(state: ViewState): void {
+  const tableIcon = document.querySelector<HTMLButtonElement>(
+    ".header-table-icon-container",
+  );
+  const mapIcon = document.querySelector<HTMLButtonElement>(
+    ".header-map-icon-container",
+  );
+  const tableView = document.querySelector<HTMLElement>("#table-view");
+  const mapView = document.querySelector<HTMLElement>("#map");
+  const mapCounter = document.querySelector<HTMLElement>("#counter");
+  if (state === "map") {
+    tableIcon.style.display = "inline-flex";
+    mapIcon.style.display = "none";
+    tableView.hidden = true;
+    mapView.hidden = false;
+    mapCounter.hidden = false;
+  } else {
+    tableIcon.style.display = "none";
+    mapIcon.style.display = "inline-flex";
+    tableView.hidden = false;
+    mapView.hidden = true;
+    mapCounter.hidden = true;
+  }
+}
+
+function updateOnIconClick(observable: ViewStateObservable): void {
+  const tableIcon = document.querySelector<HTMLButtonElement>(
+    ".header-table-icon-container",
+  );
+  tableIcon.addEventListener("click", () => observable.setValue("table"));
+
+  const mapIcon = document.querySelector<HTMLButtonElement>(
+    ".header-map-icon-container",
+  );
+  mapIcon.addEventListener("click", () => observable.setValue("map"));
+}
+
+export default function initViewToggle(): void {
+  const viewToggle = new Observable<ViewState>("map");
+  viewToggle.subscribe(updateUI);
+  updateOnIconClick(viewToggle);
+  viewToggle.initialize();
+}
