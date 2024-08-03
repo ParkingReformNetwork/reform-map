80a9f62b51b6be9790c79ef589b985bb9750d048
Eric Arellano <ericarellano@me.com>
2024-08-03 10:53:44 -0400

    Allow sorting

--- src/js/table.ts
+++ src/js/table.ts
@@ -1,7 +1,13 @@
-import { Tabulator } from "tabulator-tables";
+import { Tabulator, SortModule } from "tabulator-tables";
 import "tabulator-tables/dist/css/tabulator.min.css";
 
 export default function initTable(): Tabulator {
+  // TODO: freeze columns? https://tabulator.info/docs/6.2/layout#frozen-column
+  // TODO: allow resizing columns? https://tabulator.info/docs/6.2/modules#module-resizeColumns
+  // TODO: moveable columns? https://tabulator.info/docs/6.2/modules#module-moveColumn
+  // TODO: responsive layout https://tabulator.info/docs/6.2/modules#module-responsiveLayout
+  Tabulator.registerModule([SortModule]);
+
   // TODO: load data from PlaceFilterManager.
   const table = new Tabulator("#table", {
     data: [
@@ -26,6 +32,7 @@ export default function initTable(): Tabulator {
   // TODO: subscribe to updates to the selected cities. Figure out
   // how to ensure the table is already initialized. Also consider using
   // https://tabulator.info/docs/6.2/reactivity
-
+  //
+  // Is the best approach to filter? https://tabulator.info/docs/6.2/filter
   return table;
 }
