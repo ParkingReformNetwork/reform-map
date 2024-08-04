2838fa1b33f95e9408b635b692c08536786d5928
Eric Arellano <ericarellano@me.com>
2024-08-04 11:20:21 -0400

    Inline function

--- src/js/mapPosition.ts
+++ src/js/mapPosition.ts
@@ -2,14 +2,6 @@ import { Map } from "leaflet";
 
 import { PlaceFilterManager } from "./FilterState";
 
-function snapToPlace(map: Map, lat: string, long: string): void {
-  map.setView(
-    // @ts-ignore
-    [lat, long],
-    6,
-  );
-}
-
 export default function subscribeSnapToPlace(
   manager: PlaceFilterManager,
   map: Map,
@@ -17,7 +9,11 @@ export default function subscribeSnapToPlace(
   manager.subscribe(({ searchInput }) => {
     if (searchInput) {
       const entry = manager.entries[searchInput];
-      snapToPlace(map, entry.lat, entry.long);
+      map.setView(
+        // @ts-ignore
+        [entry.lat, entry.long],
+        6,
+      );
     }
   });
 }
