6d92e8101464ba6fcc3f678bc6a66edd52bbaa37
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-08-03 20:06:43 -0400

    Set up remaining columns for table (#439)
    Some decisions:
    
    * Don't show the description. It would make it impossible to fit
    everything in one line, which gets in the way of scanning the table
    * Call it "Place" rather than "City" to account for
    states/countries/counties. This will need to be improved throughout the
    data model in general, but this is a good first step. Note that the
    Place is always defined in our dataset currently.
    * Freeze the Place column because this is useful on mobile
    * Don't hide any rows on mobile - it may still be useful to see them
    * Let the users resize and rearrange columns
    * Use fixed width, and don't show the entire row by default. The edge
    cases make the table too sparse otherwise if we want to fit everything.
    Users can either resize the column or go to the details page
    
    <img width="1374" alt="Screenshot 2024-08-03 at 7 56 34 PM"
    src="https://github.com/user-attachments/assets/14fe5ce5-8bd3-4e47-85e8-6c190253d380">
    
    <img width="376" alt="Screenshot 2024-08-03 at 7 56 48 PM"
    src="https://github.com/user-attachments/assets/ea8cdeb3-dca2-405d-ac86-3959c8ce35a8">
    
    The app is too slow when there are lots of rows. It was slow before, and
    this PR makes it even worse where it hangs. This needs to be fixed
    before the table view goes live.

--- src/js/table.ts
+++ src/js/table.ts
@@ -1,38 +1,111 @@
-import { Tabulator, FilterModule, SortModule } from "tabulator-tables";
+import {
+  Tabulator,
+  FilterModule,
+  FormatModule,
+  SortModule,
+  ResizeColumnsModule,
+  MoveColumnsModule,
+  ColumnDefinition,
+  FrozenColumnsModule,
+} from "tabulator-tables";
 import "tabulator-tables/dist/css/tabulator.min.css";
+import { DateTime } from "luxon";
 
 import { PlaceFilterManager } from "./FilterState";
+
+function compareDates(a: string, b: string): number {
+  const dateA = DateTime.fromFormat(a, "LLL d, yyyy");
+  const dateB = DateTime.fromFormat(b, "LLL d, yyyy");
+  if (!dateA.isValid) return 1;
+  if (!dateB.isValid) return -1;
+  return dateA.valueOf() - dateB.valueOf();
+}
 
 export default function initTable(
   filterManager: PlaceFilterManager,
 ): Tabulator {
-  // TODO: limit column width for state & country
-  // TODO: figure out how to display details
-
-  // TODO: freeze columns? https://tabulator.info/docs/6.2/layout#frozen-column
-  // TODO: allow resizing columns? https://tabulator.info/docs/6.2/modules#module-resizeColumns
-  // TODO: moveable columns? https://tabulator.info/docs/6.2/modules#module-moveColumn
-  // TODO: responsive layout https://tabulator.info/docs/6.2/modules#module-responsiveLayout
-  Tabulator.registerModule([FilterModule, SortModule]);
+  Tabulator.registerModule([
+    FilterModule,
+    FormatModule,
+    FrozenColumnsModule,
+    SortModule,
+    ResizeColumnsModule,
+    MoveColumnsModule,
+  ]);
 
   const data = Object.entries(filterManager.entries).map(
     ([placeId, entry]) => ({
       id: placeId,
-      city: entry.city,
+      place: entry.city,
       state: entry.state,
       country: entry.country,
+      population: parseInt(entry.population).toLocaleString("en-us"),
+      date: entry.date_of_reform,
+      citationUrl: entry.citation_url,
+      status: entry.report_status,
+      landUse: entry.land_uses,
+      policyChange: entry.report_type,
+      scope: entry.report_magnitude,
     }),
   );
-  const columns = [
-    { title: "City", field: "city" },
-    { title: "State", field: "state" },
-    { title: "Country", field: "country" },
+  const columns: ColumnDefinition[] = [
+    {
+      title: "Place",
+      field: "place",
+      width: 180,
+      frozen: true,
+      formatter: "link",
+      formatterParams: {
+        urlField: "citationUrl",
+        labelField: "place",
+        target: "_blank",
+      },
+    },
+    { title: "State", field: "state", width: 70 },
+    { title: "Country", field: "country", width: 70 },
+    {
+      title: "Population",
+      field: "population",
+      sorter: "number",
+      sorterParams: {
+        // @ts-ignore
+        thousandSeparator: ",",
+      },
+      width: 90,
+    },
+    {
+      title: "Date",
+      field: "date",
+      width: 110,
+      sorter: compareDates,
+    },
+    {
+      title: "Policy change",
+      field: "policyChange",
+      width: 260,
+    },
+    {
+      title: "Scope",
+      field: "scope",
+      width: 260,
+    },
+    {
+      title: "Land use",
+      field: "landUse",
+      width: 160,
+    },
+    {
+      title: "Status",
+      field: "status",
+      width: 120,
+    },
   ];
 
   const table = new Tabulator("#table", {
     data,
     columns,
     layout: "fitColumns",
+    movableColumns: true,
   });
 
   let tableBuilt = false;
