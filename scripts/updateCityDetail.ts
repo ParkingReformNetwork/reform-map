3f7fc5acab932f14a4452b1b41e8919f3196563b
Eric Arellano <ericarellano@me.com>
2024-08-31 15:19:47 -0400

    Revamp data model for update-city-detail

--- scripts/updateCityDetail.ts
+++ scripts/updateCityDetail.ts
@@ -10,29 +10,29 @@ import Handlebars from "handlebars";
 import { DateTime } from "luxon";
 
 export type PlaceEntry = {
-  City: string;
-  State: string;
-  Summary: string;
-  "Verified By": string;
-  "Source Description": string;
-  Type: string;
-  URL: string;
-  Notes: string;
-  Attachments: string;
-  "Last updated": string;
-  "Create Time": string;
-  Status: string;
-  Uses: string;
-  "Reform Type": string;
-  Magnitude: string;
-  Requirements: string;
-  Reporter: string;
-  "Report Last updated": string;
-  "City Last Updated": string;
-  CitationID: string;
+  summary: string;
+  status: string;
+  policyChange: string;
+  landUse: string;
+  scope: string;
+  requirements: string;
+  reporter: string;
+  reportLastUpdated: DateTime<true>;
+  cityLastUpdated: DateTime<true>;
+  citations: Citation[];
 };
 
-type NormalizedAttachment = {
+type Citation = {
+  idx: number;
+  description: string;
+  type: string;
+  url: string;
+  notes: string;
+  lastUpdated: DateTime<true>;
+  attachments: Attachment[];
+};
+
+type Attachment = {
   url: string;
   fileName: string;
   isDoc: boolean;
@@ -47,6 +47,10 @@ const TIME_ZONE_MAPPING: Partial<Record<string, string>> = {
   PDT: "America/Los_Angeles",
   PST: "America/Los_Angeles",
 };
+
+function rmSpace(v: string): string {
+  return v.replace(/ /g, "");
+}
 
 export function parseDatetime(
   val: string,
@@ -67,7 +71,7 @@ export function parseDatetime(
   return result;
 }
 
-async function fetchData(): Promise<PlaceEntry[]> {
+async function fetchGTablesData(): Promise<Record<string, any>[]> {
   const response = await fetch(
     "https://area120tables.googleapis.com/link/aUJhBkwwY9j1NpD-Enh4WU/export?key=aasll5u2e8Xf-jxNNGlk3vbnOYcDsJn-JbgeI3z6IkPk8z5CxpWOLEp5EXd8iMF_bc",
     {
@@ -75,24 +79,63 @@ async function fetchData(): Promise<PlaceEntry[]> {
     },
   );
   const rawData = await response.text();
-  return Papa.parse(rawData, { header: true }).data as PlaceEntry[];
+  return Papa.parse(rawData, { header: true }).data as Record<string, any>[];
+}
+
+async function loadData(): Promise<Record<string, PlaceEntry>> {
+  // Google Tables stores one row per citation. A place may have >1 citation,
+  // but they share the same PlaceEntry data otherwise.
+  const gTablesData = await fetchGTablesData();
+
+  const result: Record<string, PlaceEntry> = {};
+  gTablesData.forEach((row) => {
+    if (!row.City) return;
+
+    const placeId = row.State ? `${row.City}_${row.State}` : row.City;
+    const citationIdx = result[placeId]
+      ? result[placeId].citations.length + 1
+      : 1;
+
+    const citation = {
+      idx: citationIdx,
+      description: row["Source Description"],
+      type: row.Type,
+      url: row.URL,
+      notes: row.Notes,
+      lastUpdated: parseDatetime(row["Last updated"]),
+      attachments: normalizeAttachments(row.Attachments, citationIdx, placeId),
+    };
+
+    if (result[placeId]) {
+      result[placeId].citations.push(citation);
+      return;
+    }
+
+    result[placeId] = {
+      summary: row.Summary,
+      status: row.Status,
+      policyChange: row["Reform Type"],
+      landUse: row.Uses,
+      scope: row.Magnitude,
+      requirements: row.Requirements,
+      reporter: row.Reporter,
+      reportLastUpdated: parseDatetime(row["Report Last updated"]),
+      cityLastUpdated: parseDatetime(row["City Last Updated"]),
+      citations: [citation],
+    };
+  });
+
+  return result;
 }
 
 export function needsUpdate(
-  placeEntries: PlaceEntry[],
+  entry: PlaceEntry,
   globalLastUpdated: DateTime<true>,
 ): boolean {
-  const lastUpdatedDates = placeEntries.map((row) =>
-    parseDatetime(row["Last updated"]),
-  );
-  const reportLastUpdated = parseDatetime(
-    placeEntries[0]["Report Last updated"],
-  );
-  const cityLastUpdated = parseDatetime(placeEntries[0]["City Last Updated"]);
   const maxLastUpdated = DateTime.max(
-    ...lastUpdatedDates,
-    reportLastUpdated,
-    cityLastUpdated,
+    ...entry.citations.map((x) => x.lastUpdated),
+    entry.reportLastUpdated,
+    entry.cityLastUpdated,
   );
   return maxLastUpdated >= globalLastUpdated;
 }
@@ -101,34 +144,29 @@ export function needsUpdate(
  Rewrite the entries' attachments field to a normalized object.
  */
 export function normalizeAttachments(
-  placeEntries: PlaceEntry[],
-  placeIdNoSpace: string,
-): NormalizedAttachment[] {
-  return placeEntries.reduce(
-    (result: NormalizedAttachment[], entry: PlaceEntry, i: number) => {
-      if (!entry.Attachments) {
-        return result;
-      }
-      const attachments = entry.Attachments.split(/\s+/).map((val, j) => {
-        const fileTypeMatch = val.match(/\.[a-zA-Z_]+$/);
-        const fileType = fileTypeMatch ? fileTypeMatch[0] : null;
-        return {
-          url: val,
-          fileName: new URL(val).pathname.split("/").pop()!,
-          isDoc: fileType === ".docx" || fileType === ".pdf",
-          outputPath: `attachment_images/${placeIdNoSpace}_${i + 1}_${
-            j + 1
-          }${fileType}`,
-        };
-      });
-      return [...result, ...attachments];
-    },
-    [],
-  );
+  attachments: string,
+  citationIdx: number,
+  placeId: string,
+): Attachment[] {
+  if (!attachments) {
+    return [];
+  }
+  return attachments.split(/\s+/).map((val, j) => {
+    const fileTypeMatch = val.match(/\.[a-zA-Z_]+$/);
+    const fileType = fileTypeMatch ? fileTypeMatch[0] : null;
+    return {
+      url: val,
+      fileName: new URL(val).pathname.split("/").pop()!,
+      isDoc: fileType === ".docx" || fileType === ".pdf",
+      outputPath: `attachment_images/${rmSpace(placeId)}_${citationIdx}_${
+        j + 1
+      }${fileType}`,
+    };
+  });
 }
 
 async function setupAttachmentDownloads(
-  attachments: NormalizedAttachment[],
+  attachments: Attachment[],
 ): Promise<void> {
   // Use await in a for loop to avoid making too many calls -> rate limiting.
   for (const attachment of attachments) {
@@ -144,53 +182,42 @@ async function setupAttachmentDownloads(
 }
 
 function renderHandlebars(
-  placeEntries: PlaceEntry[],
+  placeId: string,
+  entry: PlaceEntry,
   template: HandlebarsTemplateDelegate,
 ): string {
-  const citations = placeEntries.map((entry, i) => ({
-    idx: i + 1,
-    sourceDescription: entry["Source Description"],
-    type: entry.Type,
-    notes: entry.Notes,
-    url: entry.URL,
-    attachments: entry.Attachments,
-  }));
-  // The entries duplicate a lot of cells, so it's safe to simply look at the first
-  // entry to get the following information.
-  const entry0 = placeEntries[0];
   return template({
-    city: entry0.City,
-    state: entry0.State ? `, ${entry0.State}` : "",
-    summary: entry0.Summary,
-    status: entry0.Status,
-    reformType: entry0["Reform Type"],
-    uses: entry0.Uses,
-    magnitude: entry0.Magnitude,
-    requirements: entry0.Requirements,
-    reporter: entry0.Reporter,
-    citations,
+    placeId,
+    summary: entry.summary,
+    status: entry.status,
+    reformType: entry.policyChange,
+    landUse: entry.landUse,
+    scope: entry.scope,
+    requirements: entry.requirements,
+    reporter: entry.reporter,
+    citations: entry.citations,
   });
 }
 
 async function processPlace(
   placeId: string,
-  placeEntries: PlaceEntry[],
+  entry: PlaceEntry,
   template: HandlebarsTemplateDelegate,
   globalLastUpdated: DateTime<true>,
 ): Promise<void> {
-  if (!needsUpdate(placeEntries, globalLastUpdated)) {
+  if (!needsUpdate(entry, globalLastUpdated)) {
     console.log(`Skipping ${placeId}`);
     return;
   }
 
   console.log(`Updating ${placeId}`);
 
-  const placeIdNoSpace = placeId.replace(/ /g, "");
-  const attachments = normalizeAttachments(placeEntries, placeIdNoSpace);
-  await setupAttachmentDownloads(attachments);
+  await setupAttachmentDownloads(
+    entry.citations.flatMap((citation) => citation.attachments),
+  );
   await fs.writeFile(
-    `city_detail/${placeIdNoSpace}.html`,
-    renderHandlebars(placeEntries, template),
+    `city_detail/${rmSpace(placeId)}.html`,
+    renderHandlebars(placeId, entry, template),
   );
 }
 
@@ -207,25 +234,14 @@ async function main(): Promise<void> {
   const [rawGlobalLastUpdated, rawTemplate, data] = await Promise.all([
     fs.readFile(GLOBAL_LAST_UPDATED_FP, "utf-8"),
     fs.readFile("scripts/city_detail.html.handlebars", "utf-8"),
-    fetchData(),
+    loadData(),
   ]);
   const template = Handlebars.compile(rawTemplate);
   const globalLastUpdated = parseDatetime(rawGlobalLastUpdated, false);
 
-  // A place will have one entry per citation.
-  const entriesByPlaceId: Record<string, PlaceEntry[]> = {};
-  data.forEach((row) => {
-    if (!row.City) return;
-    const placeId = row.State ? `${row.City}_${row.State}` : row.City;
-    if (!entriesByPlaceId[placeId]) {
-      entriesByPlaceId[placeId] = [];
-    }
-    entriesByPlaceId[placeId].push(row);
-  });
-
   // Use await in a for loop to avoid making too many calls -> rate limiting.
-  for (const [placeId, placeEntries] of Object.entries(entriesByPlaceId)) {
-    await processPlace(placeId, placeEntries, template, globalLastUpdated);
+  for (const [placeId, entry] of Object.entries(data)) {
+    await processPlace(placeId, entry, template, globalLastUpdated);
   }
 
   await updateLastUpdatedFile();
