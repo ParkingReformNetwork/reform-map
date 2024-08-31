bdeba4b34277a2dbbe25fa992ab8f717dc667d09
Eric Arellano <ericarellano@me.com>
2024-08-31 12:36:10 -0400

    Add docstring

--- src/js/types.ts
+++ src/js/types.ts
@@ -3,8 +3,12 @@ import type { DateTime } from "luxon";
 export type PlaceId = string;
 
 export interface PlaceEntry {
+  // Full name of the town, city, county, province, state, or country.
+  // Must always be set.
   place: string;
+  // State or province abbreviation. Not set for countries.
   state: string | null;
+  // Country abbreviation. Must always be set.
   country: string;
   summary: string;
   status: string;
