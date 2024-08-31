98f8e30981cfc806d6adf2f8132990f2f891c949
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-08-31 17:20:31 -0400

    Revamp data model for update-city-detail (#489)
    The modeling is complicated because a place may have >1 citation. The
    Google Table we use returns a row per citation, and those rows duplicate
    a lot of the core data about the city. Before, we were handling each row
    in the script throughout its lifetime.
    
    Now, we better pre-process the data to have one entry per place. The
    data now gets put into the shape of our Handlebars template.
    
    This change will unblock us storing the supplemental information we need
    to generate HTML pages into a JSON file.

--- tests/scripts/updateCityDetail.test.ts
+++ tests/scripts/updateCityDetail.test.ts
@@ -12,130 +12,125 @@ import { readDataCsv } from "../../scripts/syncLatLng";
 
 test.describe("needsUpdate()", () => {
   test("returns false if everything is older than globalLastUpdated", () => {
-    const entryTime = "May 5, 2023, 8:00:00 AM PDT";
-    const entries = [
-      {
-        "Last updated": entryTime,
-        "Report Last updated": entryTime,
-        "City Last Updated": entryTime,
-      },
-      {
-        "Last updated": "May 4, 2023, 8:00:00 AM PDT",
-        "Report Last updated": entryTime,
-        "City Last Updated": entryTime,
-      },
-    ] as PlaceEntry[];
+    const entryTime = parseDatetime("May 5, 2023, 8:00:00 AM PDT");
+    const entry = {
+      reportLastUpdated: entryTime,
+      cityLastUpdated: entryTime,
+      citations: [
+        {
+          lastUpdated: entryTime,
+        },
+      ],
+    } as PlaceEntry;
     const globalLastUpdated = parseDatetime("May 8, 2023, 8:00:00 AM PDT");
-    expect(needsUpdate(entries, globalLastUpdated)).toBe(false);
+    expect(needsUpdate(entry, globalLastUpdated)).toBe(false);
   });
 
   test("returns true if the report has been updated recently", () => {
-    const entryTime = "May 5, 2023, 8:00:00 AM PDT";
-    const entries = [
-      {
-        "Last updated": entryTime,
-        "Report Last updated": "May 10, 2023, 8:00:00 AM PDT",
-        "City Last Updated": entryTime,
-      },
-      {
-        "Last updated": entryTime,
-        "Report Last updated": "May 10, 2023, 8:00:00 AM PDT",
-        "City Last Updated": entryTime,
-      },
-    ] as PlaceEntry[];
+    const entryTime = parseDatetime("May 5, 2023, 8:00:00 AM PDT");
+    const entry = {
+      reportLastUpdated: parseDatetime("May 10, 2023, 8:00:00 AM PDT"),
+      cityLastUpdated: entryTime,
+      citations: [
+        {
+          lastUpdated: entryTime,
+        },
+      ],
+    } as PlaceEntry;
     const globalLastUpdated = parseDatetime("May 6, 2023, 8:00:00 AM PDT");
-    expect(needsUpdate(entries, globalLastUpdated)).toBe(true);
+    expect(needsUpdate(entry, globalLastUpdated)).toBe(true);
   });
 
   test("returns true if the city has been updated recently", () => {
-    const entryTime = "May 5, 2023, 8:00:00 AM PDT";
-    const entries = [
-      {
-        "Last updated": entryTime,
-        "Report Last updated": entryTime,
-        "City Last Updated": "May 10, 2023, 8:00:00 AM PDT",
-      },
-      {
-        "Last updated": entryTime,
-        "Report Last updated": entryTime,
-        "City Last Updated": "May 10, 2023, 8:00:00 AM PDT",
-      },
-    ] as PlaceEntry[];
+    const entryTime = parseDatetime("May 5, 2023, 8:00:00 AM PDT");
+    const entry = {
+      reportLastUpdated: entryTime,
+      cityLastUpdated: parseDatetime("May 10, 2023, 8:00:00 AM PDT"),
+      citations: [
+        {
+          lastUpdated: entryTime,
+        },
+      ],
+    } as PlaceEntry;
     const globalLastUpdated = parseDatetime("May 6, 2023, 8:00:00 AM PDT");
-    expect(needsUpdate(entries, globalLastUpdated)).toBe(true);
+    expect(needsUpdate(entry, globalLastUpdated)).toBe(true);
   });
 
   test("returns true if at least one of the citations has been updated recently", () => {
-    const entryTime = "May 5, 2023, 8:00:00 AM PDT";
-    const entries = [
-      {
-        "Last updated": entryTime,
-        "Report Last updated": entryTime,
-        "City Last Updated": entryTime,
-      },
-      {
-        "Last updated": "May 10, 2023, 8:00:00 AM PDT",
-        "Report Last updated": entryTime,
-        "City Last Updated": entryTime,
-      },
-    ] as PlaceEntry[];
+    const entryTime = parseDatetime("May 5, 2023, 8:00:00 AM PDT");
+    const entry = {
+      reportLastUpdated: entryTime,
+      cityLastUpdated: entryTime,
+      citations: [
+        {
+          lastUpdated: entryTime,
+        },
+        {
+          lastUpdated: parseDatetime("May 10, 2023, 8:00:00 AM PDT"),
+        },
+      ],
+    } as PlaceEntry;
     const globalLastUpdated = parseDatetime("May 6, 2023, 8:00:00 AM PDT");
-    expect(needsUpdate(entries, globalLastUpdated)).toBe(true);
+    expect(needsUpdate(entry, globalLastUpdated)).toBe(true);
   });
 
   test("can handle different time zones", () => {
-    const entryTime = "May 5, 2023, 8:00:00 AM PDT";
-    const entries = [
-      {
-        "Last updated": "May 6, 2023, 12:00:00 PM PDT",
-        "Report Last updated": entryTime,
-        "City Last Updated": entryTime,
-      },
-      {
-        "Last updated": entryTime,
-        "Report Last updated": entryTime,
-        "City Last Updated": entryTime,
-      },
-    ] as PlaceEntry[];
+    const entryTime = parseDatetime("May 5, 2023, 8:00:00 AM PDT");
+    const entry = {
+      reportLastUpdated: entryTime,
+      cityLastUpdated: parseDatetime("May 6, 2023, 12:00:00 PM PDT"),
+      citations: [
+        {
+          lastUpdated: entryTime,
+        },
+      ],
+    } as PlaceEntry;
     // Even though 11 AM is naively earlier than 12 PM, due to time zones, the globalLastUpdated
     // happens after any of the city updates.
     let globalLastUpdated = parseDatetime(
       "May 6, 2023, 11:00:00 AM Pacific/Honolulu",
       false,
     );
-    expect(needsUpdate(entries, globalLastUpdated)).toBe(false);
+    expect(needsUpdate(entry, globalLastUpdated)).toBe(false);
 
-    // To be extra sure, we ensure that the time zone would return true.
+    // To be extra sure, we ensure that the same time zone would return true.
     globalLastUpdated = parseDatetime("May 6, 2023, 11:00:00 AM PDT");
-    expect(needsUpdate(entries, globalLastUpdated)).toBe(true);
+    expect(needsUpdate(entry, globalLastUpdated)).toBe(true);
   });
 });
 
 test("normalizeAttachments() converts string entries into objects", () => {
-  const input = [
-    { Attachments: "" },
-    { Attachments: "https://prn.org/photo1.png" },
-    { Attachments: "https://prn.org/doc1.pdf https://prn.org/img2.jpg" },
-  ] as PlaceEntry[];
-  const result = normalizeAttachments(input, "MyCity_AZ");
-  expect(result).toEqual([
+  expect(normalizeAttachments("", 1, "My City, AZ")).toEqual([]);
+
+  expect(
+    normalizeAttachments("https://prn.org/photo1.png", 1, "My City, AZ"),
+  ).toEqual([
     {
       url: "https://prn.org/photo1.png",
       fileName: "photo1.png",
       isDoc: false,
-      outputPath: "attachment_images/MyCity_AZ_2_1.png",
+      outputPath: "attachment_images/MyCity_AZ_1_1.png",
     },
+  ]);
+
+  expect(
+    normalizeAttachments(
+      "https://prn.org/doc1.pdf https://prn.org/img2.jpg",
+      2,
+      "My City, AZ",
+    ),
+  ).toEqual([
     {
       url: "https://prn.org/doc1.pdf",
       fileName: "doc1.pdf",
       isDoc: true,
-      outputPath: "attachment_images/MyCity_AZ_3_1.pdf",
+      outputPath: "attachment_images/MyCity_AZ_2_1.pdf",
     },
     {
       url: "https://prn.org/img2.jpg",
       fileName: "img2.jpg",
       isDoc: false,
-      outputPath: "attachment_images/MyCity_AZ_3_2.jpg",
+      outputPath: "attachment_images/MyCity_AZ_2_2.jpg",
     },
   ]);
 });
