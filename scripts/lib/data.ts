2811ca3c6434588a011c273c6c745223813b8202
Eric Arellano <ericarellano@me.com>
2024-08-31 21:53:12 -0400

    Generate CSV from JSON

--- scripts/lib/data.ts
+++ scripts/lib/data.ts
@@ -2,7 +2,36 @@ import fs from "fs/promises";
 
 import { RawEntry, PlaceId } from "../../src/js/types";
 
+export type ExtendedEntry = {
+  reporter: string | null;
+  updated: string;
+};
+
+export type CompleteEntry = RawEntry & ExtendedEntry;
+
 export async function readCoreData(): Promise<Record<PlaceId, RawEntry>> {
   const raw = await fs.readFile("data/core.json", "utf8");
   return JSON.parse(raw);
+}
+
+export async function readExtendedData(): Promise<
+  Record<PlaceId, ExtendedEntry>
+> {
+  const raw = await fs.readFile("data/extended.json", "utf8");
+  return JSON.parse(raw);
+}
+
+export async function readCompleteData(): Promise<
+  Record<PlaceId, CompleteEntry>
+> {
+  const [coreData, extendedData] = await Promise.all([
+    readCoreData(),
+    readExtendedData(),
+  ]);
+  return Object.fromEntries(
+    Object.entries(coreData).map(([placeId, core]) => [
+      placeId,
+      { ...core, ...extendedData[placeId] },
+    ]),
+  );
 }
