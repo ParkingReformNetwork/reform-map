0b3ccfddf5960581b9fbd4a22493b51a5f19af05
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-08-31 22:09:48 -0400

    Generate CSV from JSON (#495)
    Follow up to
    https://github.com/ParkingReformNetwork/reform-map/pull/494. This will
    allow us to make JSON the source of truth, rather than `map/data.csv`.
    
    Note that we can now be much more flexible with what we put in the CSV.
    In the future, we can add more data to the CSV without worrying about
    bloating our runtime app. We also now do data transformations like how
    we handle booleans.

---
+++ scripts/generateCsv.ts
@@ -0,0 +1,38 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+
+import fs from "fs/promises";
+
+import Papa from "papaparse";
+
+import { readCompleteData } from "./lib/data";
+
+async function main(): Promise<void> {
+  const completeData = await readCompleteData();
+  const data = Object.values(completeData).map((row) => ({
+    place: row.place,
+    state: row.state,
+    country: row.country,
+    all_minimums_repealed: row.repeal ? "TRUE" : "FALSE",
+    status: row.status,
+    policy_change: row.policy,
+    scope: row.scope,
+    land_uses: row.land,
+    reform_date: row.date,
+    population: row.pop,
+    lat: row.coord[0],
+    long: row.coord[1],
+    url: row.url,
+    reporter: row.reporter,
+    summary: row.summary,
+  }));
+  const csv = Papa.unparse(Object.values(data));
+  await fs.writeFile("data/data.csv", csv);
+}
+
+if (process.env.NODE_ENV !== "test") {
+  main().catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+}
