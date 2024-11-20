78f2a531f8c5cb645013a95660788f0f0dd1f246
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-11-20 09:54:46 -0500

    Add tests for date class (#544)
    Meant to be added in
    https://github.com/ParkingReformNetwork/reform-map/pull/543

---
+++ tests/app/types.test.ts
@@ -0,0 +1,15 @@
+import { expect, test } from "@playwright/test";
+
+import { Date } from "../../src/js/types";
+
+test("Date.format()", () => {
+  expect(new Date("2020").format()).toEqual("2020");
+  expect(new Date("2020-02").format()).toEqual("Feb 2020");
+  expect(new Date("2020-02-13").format()).toEqual("Feb 13, 2020");
+});
+
+test("Date.preposition()", () => {
+  expect(new Date("2020").preposition()).toEqual("in");
+  expect(new Date("2020-02").preposition()).toEqual("in");
+  expect(new Date("2020-02-13").preposition()).toEqual("on");
+});
