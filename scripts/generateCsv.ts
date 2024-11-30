37168345e730aa5da91d864234655d78bd458c53
Eric Arellano <ericarellano@me.com>
2024-11-30 13:57:45 -0500

    `gen-csv` handles multiple policy records

--- scripts/generateCsv.ts
+++ scripts/generateCsv.ts
@@ -3,25 +3,27 @@
 
 import fs from "fs/promises";
 
+import { sum } from "lodash-es";
 import Papa from "papaparse";
 
-import { readProcessedCompleteData } from "./lib/data";
+import {
+  ProcessedCompleteEntry,
+  ProcessedCompletePolicy,
+  readProcessedCompleteData,
+} from "./lib/data";
 
-function assertExpectedNumEntries(jsonKeys: string[], csv: string): void {
-  const numJson = jsonKeys.length;
-  const numCsv = csv.split("\r\n").length - 1;
-  if (numJson !== numCsv) {
-    throw new Error(`CSV has unequal entries to JSON: ${numCsv} vs ${numJson}`);
-  }
+const DELIMITER = "; ";
+
+function toBoolean(condition: boolean): string {
+  return condition ? "TRUE" : "FALSE";
 }
 
-async function main(): Promise<void> {
-  const completeData = await readProcessedCompleteData();
-  const data = Object.values(completeData).map((entry) => ({
+function createLegacyCsv(data: ProcessedCompleteEntry[]): string {
+  const entries = data.map((entry) => ({
     place: entry.place.name,
     state: entry.place.state,
     country: entry.place.country,
-    all_minimums_repealed: entry.place.repeal ? "TRUE" : "FALSE",
+    all_minimums_repealed: toBoolean(entry.place.repeal),
     status: entry.unifiedPolicy.status,
     policy_change: entry.unifiedPolicy.policy,
     scope: entry.unifiedPolicy.scope,
@@ -34,14 +36,107 @@ async function main(): Promise<void> {
     reporter: entry.unifiedPolicy.reporter,
     summary: entry.unifiedPolicy.summary,
   }));
-  const csv = Papa.unparse(Object.values(data));
-  assertExpectedNumEntries(Object.keys(completeData), csv);
-  await fs.writeFile("data/data.csv", csv);
-  console.log("Generated CSV at data/data.csv");
+  const csv = Papa.unparse(entries);
 
-  // Also save to map/data.csv for now, until data/data.csv can be deployed to the PRN site.
-  await fs.writeFile("map/data.csv", csv);
-  console.log("Generated CSV at map/data.csv");
+  // Validate expected number of entries.
+  const numJson = data.length;
+  const numCsv = csv.split("\r\n").length - 1;
+  if (numJson !== numCsv) {
+    throw new Error(`CSV has unequal entries to JSON: ${numCsv} vs ${numJson}`);
+  }
+
+  return csv;
+}
+
+function createAnyPolicyCsv(data: ProcessedCompleteEntry[]): string {
+  const entries = data.map((entry) => ({
+    place: entry.place.name,
+    state: entry.place.state,
+    country: entry.place.country,
+    population: entry.place.pop,
+    lat: entry.place.coord[1],
+    long: entry.place.coord[0],
+    all_minimums_repealed: toBoolean(entry.place.repeal),
+    has_minimums_repeal: toBoolean(!!entry.rm_min?.length),
+    has_minimums_reduction: toBoolean(!!entry.reduce_min?.length),
+    has_maximums: toBoolean(!!entry.add_max?.length),
+    prn_url: entry.place.url,
+  }));
+  const csv = Papa.unparse(entries);
+
+  // Validate expected number of entries.
+  const numJson = data.length;
+  const numCsv = csv.split("\r\n").length - 1;
+  if (numJson !== numCsv) {
+    throw new Error(`CSV has unequal entries to JSON: ${numCsv} vs ${numJson}`);
+  }
+
+  return csv;
+}
+
+function createReformCsv(
+  data: ProcessedCompleteEntry[],
+  getter: (
+    entry: ProcessedCompleteEntry,
+  ) => ProcessedCompletePolicy[] | undefined,
+): string {
+  const entries = data.flatMap((entry) => {
+    const policies = getter(entry);
+    if (!policies) return [];
+    return policies.map((policy) => ({
+      place: entry.place.name,
+      state: entry.place.state,
+      country: entry.place.country,
+      population: entry.place.pop,
+      lat: entry.place.coord[1],
+      long: entry.place.coord[0],
+      all_minimums_repealed: toBoolean(entry.place.repeal),
+      status: policy.status,
+      reform_date: policy.date?.raw,
+      scope: policy.scope.join(DELIMITER),
+      land_uses: policy.land.join(DELIMITER),
+      requirements: policy.requirements.join(DELIMITER),
+      summary: policy.summary,
+      num_citations: policy.citations.length,
+      reporter: policy.reporter,
+      prn_url: entry.place.url,
+    }));
+  });
+  const csv = Papa.unparse(entries);
+
+  // Validate expected number of entries (total # of matching policy records).
+  const numJson = sum(data.flatMap((entry) => getter(entry)?.length ?? 0));
+  const numCsv = csv.split("\r\n").length - 1;
+  if (numJson !== numCsv) {
+    throw new Error(`CSV has unequal entries to JSON: ${numCsv} vs ${numJson}`);
+  }
+
+  return csv;
+}
+
+async function writeCsv(csv: string, filePath: string): Promise<void> {
+  await fs.writeFile(filePath, csv);
+  console.log(`Generated CSV at ${filePath}`);
+}
+
+async function main(): Promise<void> {
+  const completeData = await readProcessedCompleteData();
+  const data = Object.values(completeData);
+
+  const legacy = createLegacyCsv(data);
+  await writeCsv(legacy, "map/data.csv");
+
+  const anyPolicy = createAnyPolicyCsv(data);
+  await writeCsv(anyPolicy, "data/csv/any_parking_reform.csv");
+
+  const addMax = createReformCsv(data, (entry) => entry.add_max);
+  await writeCsv(addMax, "data/csv/add_maximums.csv");
+
+  const reduceMin = createReformCsv(data, (entry) => entry.reduce_min);
+  await writeCsv(reduceMin, "data/csv/reduce_minimums.csv");
+
+  const rmMin = createReformCsv(data, (entry) => entry.rm_min);
+  await writeCsv(rmMin, "data/csv/remove_minimums.csv");
 }
 
 if (process.env.NODE_ENV !== "test") {
