f52c8c8f3d4034fd3ce2c47f5ede800d8feaaf60
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-12-24 16:18:20 -0500

    Switch from Handlebars to Liquid (#646)
    Reasons:
    
    * Liquid is more popular with eleventy
    * I like the iteration more, which is more explicit rather than using
    the idea of an implicit context
    * Allows us to remove 7 dependencies
    * Apparently fixes the escaping of `=` in some URLs
    
    I considered using webc, but it hasn't been updated in a year and I
    think it's too experimental and high of a learning curve to be worth the
    maintenance risk.

--- eleventy.config.ts
+++ eleventy.config.ts
@@ -1,13 +1,12 @@
 /** Config for Eleventy to generate the details pages. */
 
-// @ts-ignore
-import handlebarsPlugin from "@11ty/eleventy-plugin-handlebars";
-
 import { readProcessedCompleteData } from "./scripts/lib/data.js";
 import { escapePlaceId } from "./src/js/data.js";
 
 export default async function (eleventyConfig: any) {
-  eleventyConfig.addPlugin(handlebarsPlugin);
+  eleventyConfig.setLiquidOptions({
+    jsTruthy: true,
+  });
 
   const completeData = await readProcessedCompleteData();
   eleventyConfig.addGlobalData(
@@ -22,11 +21,7 @@ export default async function (eleventyConfig: any) {
       scope: entry.unifiedPolicy.scope.join("; "),
       requirements: entry.unifiedPolicy.requirements.join("; "),
       reporter: entry.unifiedPolicy.reporter,
-      citations: entry.unifiedPolicy.citations.map((citation, i) => ({
-        idx: i + 1,
-        citationUrl: citation.url,
-        ...citation,
-      })),
+      citations: entry.unifiedPolicy.citations,
     })),
   );
 
