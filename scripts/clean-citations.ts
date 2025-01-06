a3ff2dafacd5e1e39c0a9ef9a77704b2a1b8d222
Eric Arellano <ericarellano@me.com>
2025-01-06 18:44:19 -0500

    Adapt for files

--- scripts/clean-citations.ts
+++ scripts/clean-citations.ts
@@ -1,25 +1,25 @@
-import { deleteItems } from "@directus/sdk";
+import { deleteFiles } from "@directus/sdk";
 
-import { initDirectus, readItemsBatched } from "./lib/directus";
+import { initDirectus, readItemsBatched, readCitationsFilesBatched } from "./lib/directus";
 
 async function main(): Promise<void> {
   const client = await initDirectus();
 
-  const citations = await readItemsBatched(client, "citations", ["id"], 500);
-  const allIds = citations.map(({ id }) => id);
+  const files = await readCitationsFilesBatched(client, ["id"], 400);
+  const allIds = files.map(({ id }) => id);
 
   const junctionRecords = await readItemsBatched(
     client,
-    "policy_records_citations",
-    ["citations_id"],
+    "citations_files",
+    ["directus_files_id"],
     500,
   );
   const usedIds = new Set(
-    junctionRecords.map(({ citations_id }) => citations_id),
+    junctionRecords.map(({ directus_files_id }) => directus_files_id),
   );
 
   const unusedIds = allIds.filter((id) => !usedIds.has(id));
-  await client.request(deleteItems("citations", unusedIds))
+  await client.request(deleteFiles(unusedIds))
 
   process.exit(0);
 }
