83583cc263102de014daf064427cfebcfdb5b736
Eric Arellano <ericarellano@me.com>
2025-04-05 15:21:34 -0400

    Use std lib for list conjunction

--- tests/app/data.test.ts
+++ tests/app/data.test.ts
@@ -4,7 +4,6 @@ import {
   escapePlaceId,
   placeIdToUrl,
   getFilteredIndexes,
-  joinWithConjunction,
 } from "../../src/js/model/data";
 
 test("escapePlaceID", () => {
@@ -22,26 +21,4 @@ test("getFilteredIndexes", () => {
   expect(getFilteredIndexes(["a", "b", "c"], (x) => x !== "b")).toEqual([0, 2]);
   expect(getFilteredIndexes(["a", "b", "c"], (x) => x === "b")).toEqual([1]);
   expect(getFilteredIndexes(["a", "b", "c"], (x) => x === "z")).toEqual([]);
-});
-
-test("joinWithConjunction", () => {
-  expect(joinWithConjunction([], "and")).toEqual("");
-  expect(joinWithConjunction(["apple"], "and")).toEqual("apple");
-
-  expect(joinWithConjunction(["apple", "banana"], "and")).toEqual(
-    "apple and banana",
-  );
-  expect(joinWithConjunction(["read", "write"], "or")).toEqual("read or write");
-
-  expect(joinWithConjunction(["apple", "banana", "orange"], "and")).toEqual(
-    "apple, banana, and orange",
-  );
-  expect(
-    joinWithConjunction(["read", "write", "execute", "delete"], "or"),
-  ).toEqual("read, write, execute, or delete");
-
-  expect(
-    joinWithConjunction(["  space  ", "tab\t", "\nnewline"], "and"),
-  ).toEqual("  space  , tab\t, and \nnewline");
-  expect(joinWithConjunction(["!", "@", "#"], "or")).toEqual("!, @, or #");
 });
