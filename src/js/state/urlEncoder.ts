0276a5d5fd7e8f77d9d168e5f5d3c1dd08367fb5
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2025-08-15 00:35:45 -0400

    Support encoding the rest of filter state in the URL (#791)
    Everything is now supported except for searchInput, which I don't plan
    to support.
    
    We have tests to ensure that our mappers are fully comprehensive.

--- src/js/state/urlEncoder.ts
+++ src/js/state/urlEncoder.ts
@@ -1,20 +1,31 @@
+import { isEqual } from "lodash-es";
+
 import type { FilterState } from "./FilterState";
 import { FILTER_OPTIONS } from "../filter-features/options";
 import { POPULATION_MAX_INDEX } from "../filter-features/populationSlider";
+import { COUNTRY_MAPPING } from "../model/data";
 
-export const DEFAULT_FILTER_STATE: FilterState = {
-  searchInput: null,
-  policyTypeFilter: "remove parking minimums",
-  status: "adopted",
-  allMinimumsRemovedToggle: true,
+export const MERGED_STRING_SET_OPTIONS = {
   placeType: new Set(FILTER_OPTIONS.merged.placeType),
   includedPolicyChanges: new Set(FILTER_OPTIONS.merged.includedPolicyChanges),
   scope: new Set(FILTER_OPTIONS.merged.scope),
   landUse: new Set(FILTER_OPTIONS.merged.landUse),
   country: new Set(FILTER_OPTIONS.merged.country),
   year: new Set(FILTER_OPTIONS.merged.year),
+};
+
+export const DEFAULT_FILTER_STATE: FilterState = {
+  searchInput: null,
+  policyTypeFilter: "remove parking minimums",
+  status: "adopted",
+  allMinimumsRemovedToggle: true,
+  ...MERGED_STRING_SET_OPTIONS,
   populationSliderIndexes: [0, POPULATION_MAX_INDEX],
 };
+
+const ARRAY_DELIMITER = ".";
+const BOOL_TRUE = "y";
+const BOOL_FALSE = "n";
 
 class BidirectionalMap<K extends string, V extends string> {
   private constructor(
@@ -37,31 +48,92 @@ class BidirectionalMap<K extends string, V extends string> {
     return new BidirectionalMap(encodeMap, decodeMap);
   }
 
+  keys(): Set<string> {
+    return new Set(Object.keys(this.encodeMap));
+  }
+
   encode(key: K): V {
     return this.encodeMap[key];
   }
 
-  decode(value: string | null, fallback: K): K {
+  encodeSet(keys: Set<string>): string {
+    return Array.from(keys)
+      .map((key) => this.encode(key as K))
+      .join(ARRAY_DELIMITER);
+  }
+
+  decode(value: string | null): K | null {
+    if (value === null) return null;
+    return this.decodeMap[value as V] ?? null;
+  }
+
+  decodeSet(value: string | null, fallback: Set<string>): Set<string> {
     if (value === null) return fallback;
-    return this.decodeMap[value as V] ?? fallback;
+    const parsed = new Set(
+      value
+        .split(ARRAY_DELIMITER)
+        .map((v) => this.decode(v))
+        .filter((v) => v !== null),
+    );
+    return parsed.size ? parsed : fallback;
   }
 }
 
 export const POLICY_TYPE_NAME = "reform";
 export const STATUS_NAME = "status";
+export const ALL_MINIMUMS_REPEALED_TOGGLE_NAME = "repeal";
+export const YEAR_NAME = "yr";
+export const COUNTRY_NAME = "cntry";
+export const PLACE_TYPE_NAME = "juris";
+export const INCLUDED_POLICY_NAME = "reforms";
+export const LAND_USE_NAME = "lnd";
+export const SCOPE_NAME = "scp";
+export const POPULATION_NAME = "pop";
 
-const POLICY_TYPE_MAP = BidirectionalMap.from([
+export const POLICY_TYPE_MAP = BidirectionalMap.from([
   ["any parking reform", "any"],
   ["add parking maximums", "mx"],
   ["reduce parking minimums", "rd"],
   ["remove parking minimums", "rm"],
   ["parking benefit district", "bd"],
 ]);
-const STATUS_MAP = BidirectionalMap.from([
+export const STATUS_MAP = BidirectionalMap.from([
   ["adopted", "a"],
   ["proposed", "p"],
   ["repealed", "r"],
 ]);
+export const PLACE_TYPE_MAP = BidirectionalMap.from([
+  ["city", "cty"],
+  ["state", "st"],
+  ["county", "cnty"],
+  ["country", "cntry"],
+]);
+export const LAND_USE_MAP = BidirectionalMap.from([
+  ["all uses", "all"],
+  ["commercial", "com"],
+  ["industrial", "ind"],
+  ["medical", "med"],
+  ["other", "oth"],
+  ["residential, all uses", "res-all"],
+  ["residential, low-density", "res-low"],
+  ["residential, multi-family", "fam"],
+]);
+export const SCOPE_MAP = BidirectionalMap.from([
+  ["city center / business district", "cntr"],
+  ["citywide", "cty"],
+  ["main street / special", "main"],
+  ["regional", "reg"],
+  ["transit-oriented", "trns"],
+]);
+export const COUNTRY_MAP = BidirectionalMap.from(
+  Object.entries(COUNTRY_MAPPING).map(([code, country]) => [
+    country!,
+    code.toLowerCase(),
+  ]),
+);
+export const YEAR_MAP = BidirectionalMap.from(
+  Array.from(MERGED_STRING_SET_OPTIONS.year).map((year) => [year, year]),
+);
 
 export function encodeFilterState(filterState: FilterState): URLSearchParams {
   const result = new URLSearchParams();
@@ -71,11 +143,93 @@ export function encodeFilterState(filterState: FilterState): URLSearchParams {
       POLICY_TYPE_MAP.encode(filterState.policyTypeFilter),
     );
   }
+
   if (filterState.status !== DEFAULT_FILTER_STATE.status) {
     result.append(STATUS_NAME, STATUS_MAP.encode(filterState.status));
   }
+
+  if (
+    filterState.allMinimumsRemovedToggle !==
+    DEFAULT_FILTER_STATE.allMinimumsRemovedToggle
+  ) {
+    result.append(
+      ALL_MINIMUMS_REPEALED_TOGGLE_NAME,
+      filterState.allMinimumsRemovedToggle ? BOOL_TRUE : BOOL_FALSE,
+    );
+  }
+
+  if (
+    !isEqual(
+      filterState.includedPolicyChanges,
+      DEFAULT_FILTER_STATE.includedPolicyChanges,
+    )
+  ) {
+    result.append(
+      INCLUDED_POLICY_NAME,
+      POLICY_TYPE_MAP.encodeSet(filterState.includedPolicyChanges),
+    );
+  }
+
+  if (!isEqual(filterState.placeType, DEFAULT_FILTER_STATE.placeType)) {
+    result.append(
+      PLACE_TYPE_NAME,
+      PLACE_TYPE_MAP.encodeSet(filterState.placeType),
+    );
+  }
+
+  if (!isEqual(filterState.country, DEFAULT_FILTER_STATE.country)) {
+    result.append(COUNTRY_NAME, COUNTRY_MAP.encodeSet(filterState.country));
+  }
+
+  if (!isEqual(filterState.year, DEFAULT_FILTER_STATE.year)) {
+    result.append(YEAR_NAME, YEAR_MAP.encodeSet(filterState.year));
+  }
+
+  if (!isEqual(filterState.landUse, DEFAULT_FILTER_STATE.landUse)) {
+    result.append(LAND_USE_NAME, LAND_USE_MAP.encodeSet(filterState.landUse));
+  }
+
+  if (!isEqual(filterState.scope, DEFAULT_FILTER_STATE.scope)) {
+    result.append(SCOPE_NAME, SCOPE_MAP.encodeSet(filterState.scope));
+  }
+
+  if (
+    !isEqual(
+      filterState.populationSliderIndexes,
+      DEFAULT_FILTER_STATE.populationSliderIndexes,
+    )
+  ) {
+    result.append(
+      POPULATION_NAME,
+      filterState.populationSliderIndexes.join(ARRAY_DELIMITER),
+    );
+  }
+
   result.sort();
   return result;
+}
+
+function decodeAllMinimumsRepealed(v: string | null): boolean {
+  if (v === BOOL_TRUE) return true;
+  if (v === BOOL_FALSE) return false;
+  return DEFAULT_FILTER_STATE.allMinimumsRemovedToggle;
+}
+
+export function decodePopulation(str: string | null): [number, number] {
+  if (str === null) return DEFAULT_FILTER_STATE.populationSliderIndexes;
+  let left: number;
+  let right: number;
+  try {
+    const split = str.split(ARRAY_DELIMITER);
+    if (split.length !== 2) return DEFAULT_FILTER_STATE.populationSliderIndexes;
+    left = Number.parseInt(split[0], 10);
+    right = Number.parseInt(split[1], 10);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (e) {
+    return DEFAULT_FILTER_STATE.populationSliderIndexes;
+  }
+  const isValid = left >= 0 && right <= POPULATION_MAX_INDEX && left < right;
+  return isValid ? [left, right] : DEFAULT_FILTER_STATE.populationSliderIndexes;
 }
 
 export function decodeFilterState(queryString: string): FilterState {
@@ -83,15 +237,38 @@ export function decodeFilterState(queryString: string): FilterState {
     ? queryString.slice(1)
     : queryString;
   const params = new URLSearchParams(cleanQuery);
+
   return {
-    ...DEFAULT_FILTER_STATE,
-    policyTypeFilter: POLICY_TYPE_MAP.decode(
-      params.get(POLICY_TYPE_NAME),
+    searchInput: DEFAULT_FILTER_STATE.searchInput,
+    policyTypeFilter:
+      POLICY_TYPE_MAP.decode(params.get(POLICY_TYPE_NAME)) ??
       DEFAULT_FILTER_STATE.policyTypeFilter,
+    status:
+      STATUS_MAP.decode(params.get(STATUS_NAME)) ?? DEFAULT_FILTER_STATE.status,
+    allMinimumsRemovedToggle: decodeAllMinimumsRepealed(
+      params.get(ALL_MINIMUMS_REPEALED_TOGGLE_NAME),
     ),
-    status: STATUS_MAP.decode(
-      params.get(STATUS_NAME),
-      DEFAULT_FILTER_STATE.status,
+    includedPolicyChanges: POLICY_TYPE_MAP.decodeSet(
+      params.get(INCLUDED_POLICY_NAME),
+      DEFAULT_FILTER_STATE.includedPolicyChanges,
     ),
+    year: YEAR_MAP.decodeSet(params.get(YEAR_NAME), DEFAULT_FILTER_STATE.year),
+    country: COUNTRY_MAP.decodeSet(
+      params.get(COUNTRY_NAME),
+      DEFAULT_FILTER_STATE.country,
+    ),
+    placeType: PLACE_TYPE_MAP.decodeSet(
+      params.get(PLACE_TYPE_NAME),
+      DEFAULT_FILTER_STATE.placeType,
+    ),
+    landUse: LAND_USE_MAP.decodeSet(
+      params.get(LAND_USE_NAME),
+      DEFAULT_FILTER_STATE.landUse,
+    ),
+    scope: SCOPE_MAP.decodeSet(
+      params.get(SCOPE_NAME),
+      DEFAULT_FILTER_STATE.scope,
+    ),
+    populationSliderIndexes: decodePopulation(params.get(POPULATION_NAME)),
   };
 }
