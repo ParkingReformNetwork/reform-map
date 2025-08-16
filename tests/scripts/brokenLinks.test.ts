af5bb9a09359a52e57cdac091b3b6d1747312ed8
Eric Arellano <ericarellano@me.com>
2025-08-16 18:44:44 -0400

    Update tests

--- tests/scripts/brokenLinks.test.ts
+++ tests/scripts/brokenLinks.test.ts
@@ -4,11 +4,13 @@ import { mapPlaceToCitationLinks } from "../../scripts/brokenLinks";
 test("mapPlaceToCitationLinks correctly determines some example pages", async () => {
   // If the links get updated, then update the below tests.
   const result = await mapPlaceToCitationLinks();
-  expect(result["Abilene, KS"]).toEqual([
+  expect(result["Abilene, KS, United States"]).toEqual([
     "https://abileneks.citycode.net/index.html#!artiOffStreParkLoadAndUnloRegu",
   ]);
-  expect(result["Albion, NE"]).toEqual(["https://www.albionne.com/media/686"]);
-  expect(result["Petaluma, CA"]).toEqual([
+  expect(result["Albion, NE, United States"]).toEqual([
+    "https://www.albionne.com/media/686",
+  ]);
+  expect(result["Petaluma, CA, United States"]).toEqual([
     "https://petaluma.municipal.codes/SmartCode/6.10.030",
     "https://petaluma.municipal.codes/ZoningOrds/11.035",
   ]);
