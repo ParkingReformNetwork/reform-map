059104d055f3cf0057d30bf30423080bc85e1dc4
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2025-08-17 11:24:33 -0400

    Use full state names in the U.S. and Canada (#810)
    I used a script to update Directus and core.json. That allows us to
    preserve the prior URL for the place.

--- tests/scripts/brokenLinks.test.ts
+++ tests/scripts/brokenLinks.test.ts
@@ -4,13 +4,13 @@ import { mapPlaceToCitationLinks } from "../../scripts/brokenLinks";
 test("mapPlaceToCitationLinks correctly determines some example pages", async () => {
   // If the links get updated, then update the below tests.
   const result = await mapPlaceToCitationLinks();
-  expect(result["Abilene, KS, United States"]).toEqual([
+  expect(result["Abilene, Kansas, United States"]).toEqual([
     "https://abileneks.citycode.net/index.html#!artiOffStreParkLoadAndUnloRegu",
   ]);
-  expect(result["Albion, NE, United States"]).toEqual([
+  expect(result["Albion, Nebraska, United States"]).toEqual([
     "https://www.albionne.com/media/686",
   ]);
-  expect(result["Petaluma, CA, United States"]).toEqual([
+  expect(result["Petaluma, California, United States"]).toEqual([
     "https://petaluma.municipal.codes/SmartCode/6.10.030",
     "https://petaluma.municipal.codes/ZoningOrds/11.035",
   ]);
